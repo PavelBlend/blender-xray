ddd4625e33b8afea5f877fac768244348cd178c4
Vakhurin Sergey <igelbox@gmail.com>
2014-06-16 20:18:55 +0400

    ogf dumper: add fvf_1l vertices support

--- utils/dump-ogf.py
+++ utils/dump-ogf.py
@@ -34,7 +34,14 @@ def dump_ogf4_m05(cr, out):
     pr = PackedReader(cr.next(Chunks.VERTICES))
     out('vertices: {')
     vf, vc = pr.getf('II')
-    if vf == VertexFormat.FVF_2L:
+    if vf == VertexFormat.FVF_1L:
+        out('  format:', 'fvf_2l')
+        out('  data: [')
+        for _ in range(vc):
+            out('   {v: %s, n: %s, tg: %s, bn: %s, tx: %s, b: %i' % (
+                pr.getf('fff'), pr.getf('fff'), pr.getf('fff'), pr.getf('fff'), pr.getf('ff'), pr.getf('I')[0]))
+        out('  ]')
+    elif vf == VertexFormat.FVF_2L:
         out('  format:', 'fvf_2l')
         out('  data: [')
         for _ in range(vc):
