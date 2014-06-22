961938e8bd33907d54392172f8d7fc03483f3df1
Vakhurin Sergey <igelbox@gmail.com>
2014-06-22 18:31:51 +0400

    v0.0.1

--- io_scene_xray/__init__.py
+++ io_scene_xray/__init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     'name':     'XRay Engine Tools',
     'author':   'Vakhurin Sergey (igel)',
-    'version':  (0, 0, 0),
+    'version':  (0, 0, 1),
     'blender':  (2, 7, 0),
     'category': 'Import-Export',
     'location': 'File > Import/Export',
