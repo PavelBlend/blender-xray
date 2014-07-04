b5307167c0d8bb982ab1870c20495629b132f131
Vakhurin Sergey <igelbox@gmail.com>
2014-07-04 23:51:45 +0400

    fix #23: deflate release zip files

--- mk_release.py
+++ mk_release.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
 from io_scene_xray import bl_info
-from zipfile import ZipFile
+from zipfile import ZipFile, ZIP_DEFLATED
 from os import path, walk
 
 with ZipFile('blender-xray-' + ('.'.join(map(str, bl_info['version']))) + '.zip', 'w') as z:
-    z.write('LICENSE', 'io_scene_xray/LICENSE')
+    z.write('LICENSE', 'io_scene_xray/LICENSE', compress_type=ZIP_DEFLATED)
     for root, _, files in walk('io_scene_xray'):
         for file in files:
             if not file.endswith('.py'):
                 continue
-            z.write(path.join(root, file))
+            z.write(path.join(root, file), compress_type=ZIP_DEFLATED)
