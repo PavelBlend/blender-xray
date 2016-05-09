95b3936799f3cbef86e808f468616bac6de6deae
Vakhurin Sergey <igelbox@gmail.com>
2016-05-10 00:18:32 +0300

    fix #76: export object motions: add support for 'QUATERNION'- and '<ALL> Euler' bone rotation mode
    ALSO: add support for:
    - fcurve reordering
    - missing fcurve means 0.0 keyframe value

--- io_scene_xray/xray_motions.py
+++ io_scene_xray/xray_motions.py
@@ -1,5 +1,5 @@
-from mathutils import Matrix, Euler
-from .utils import find_bone_real_parent
+from mathutils import Matrix, Euler, Quaternion
+from .utils import find_bone_real_parent, AppError
 from .xray_envelope import Behaviour, Shape
 
 
@@ -22,6 +22,40 @@ def export_motions(pw, bpy_act, cx, bpy_armature=None):
         pw.putf('B', 0)  # flags
         bpy_bone = bpy_armature.data.bones[g.name]
         rotmode = bpy_armature.pose.bones[g.name].rotation_mode
+        chs_tr, chs_rt = [None, None, None], None
+
+        def frotmatrix(rt): pass
+        if len(rotmode) == 3:
+            def frotmatrix(rt): return Euler(rt, rotmode).to_matrix()
+            chs_rt = [None, None, None]
+        elif rotmode == 'QUATERNION':
+            def frotmatrix(rt): return Quaternion(rt).to_matrix()
+            chs_rt = [None, None, None, None]
+        else:
+            raise AppError('Motions: {} bone {} uses unsupported rotation mode {}'.format(bpy_act.name, g.name, rotmode))
+
+        skipped_paths = set()
+        for c in g.channels:
+            path = c.data_path
+            chs = None
+            if path.endswith('.location'):
+                chs = chs_tr
+            elif path.endswith('.rotation_euler'):
+                if len(rotmode) == 3:
+                    chs = chs_rt
+            elif path.endswith('.rotation_quaternion'):
+                if rotmode == 'QUATERNION':
+                    chs = chs_rt
+            if chs is None:
+                if path not in skipped_paths:
+                    cx.report({'WARNING'}, 'Motions: {} bone {} has curve {} which not supported by rotation mode {}, skipping'.format(bpy_act.name, g.name, path, rotmode))
+                skipped_paths.add(path)
+                continue
+            chs[c.array_index] = c
+
+        def evaluate_channels(channels, time):
+            return (c.evaluate(time) if c else 0 for c in channels)
+
         xm = bpy_bone.matrix_local.inverted()
         real_parent = find_bone_real_parent(bpy_bone)
         if real_parent:
@@ -31,9 +65,7 @@ def export_motions(pw, bpy_act, cx, bpy_armature=None):
         xm.invert()
         envdata = []
         for t in range(int(fr[0]), int(fr[1]) + 1):
-            tr = (g.channels[0].evaluate(t), g.channels[1].evaluate(t), g.channels[2].evaluate(t))
-            rt = (g.channels[3].evaluate(t), g.channels[4].evaluate(t), g.channels[5].evaluate(t))
-            mat = xm * Matrix.Translation(tr) * Euler(rt, rotmode).to_matrix().to_4x4()
+            mat = xm * Matrix.Translation(evaluate_channels(chs_tr, t)) * frotmatrix(evaluate_channels(chs_rt, t)).to_4x4()
             tr = mat.to_translation()
             rt = mat.to_euler('ZXY')
             envdata.append((t, tr[0], tr[1], -tr[2], -rt[1], -rt[0], rt[2]))
