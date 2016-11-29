863a71be25d19f87eb7f25e64d5c2a554a4b2953
PavelBlend <stalkermodkytia@yandex.ru>
2016-11-29 22:01:23 +0300

    import level details models

--- io_scene_xray/plugin.py
+++ io_scene_xray/plugin.py
@@ -221,6 +221,51 @@ class OpImportDM(TestReadyOperator, io_utils.ImportHelper):
         return super().invoke(context, event)
 
 
+class OpImportDetails(TestReadyOperator, io_utils.ImportHelper):
+    bl_idname = 'xray_import.details'
+    bl_label = 'Import .details'
+    bl_description = 'Imports X-Ray Level Details Models (.details)'
+    bl_options = {'UNDO'}
+
+    filter_glob = bpy.props.StringProperty(default='*.details', options={'HIDDEN'})
+
+    directory = bpy.props.StringProperty(subtype="DIR_PATH")
+    files = bpy.props.CollectionProperty(type=bpy.types.OperatorFileListElement)
+
+    def execute(self, context):
+        textures_folder = plugin_prefs.get_preferences().get_textures_folder()
+        if not textures_folder:
+            self.report({'WARNING'}, 'No textures folder specified')
+        if len(self.files) == 0:
+            self.report({'ERROR'}, 'No files selected')
+            return {'CANCELLED'}
+        from .fmt_details_imp import import_file
+        from . fmt_dm_imp import ImportContext
+        cx = ImportContext(
+            report=self.report,
+            textures=textures_folder,
+            op=self,
+            bpy=bpy
+        )
+        import os.path
+        for file in self.files:
+            ext = os.path.splitext(file.name)[-1].lower()
+            if ext == '.details':
+                import_file(os.path.join(self.directory, file.name), cx)
+            else:
+                self.report({'ERROR'}, 'Format of {} not recognised'.format(file))
+        return {'FINISHED'}
+
+    def draw(self, context):
+        layout = self.layout
+        row = layout.row()
+        row.enabled = False
+        row.label('%d items' % len(self.files))
+
+    def invoke(self, context, event):
+        return super().invoke(context, event)
+
+
 class ModelExportHelper:
     selection_only = bpy.props.BoolProperty(
         name='Selection Only',
@@ -558,6 +603,7 @@ def menu_func_import(self, context):
     self.layout.operator(OpImportAnm.bl_idname, text='X-Ray animation (.anm)')
     self.layout.operator(OpImportSkl.bl_idname, text='X-Ray skeletal animation (.skl, .skls)')
     self.layout.operator(OpImportDM.bl_idname, text='X-Ray detail model (.dm)')
+    self.layout.operator(OpImportDetails.bl_idname, text='X-Ray level details models (.details)')
 
 
 def menu_func_export(self, context):
@@ -576,6 +622,7 @@ def register():
     bpy.utils.register_class(OpImportAnm)
     bpy.utils.register_class(OpImportSkl)
     bpy.utils.register_class(OpImportDM)
+    bpy.utils.register_class(OpImportDetails)
     bpy.types.INFO_MT_file_import.append(menu_func_import)
     bpy.utils.register_class(OpExportObject)
     bpy.utils.register_class(OpExportAnm)
@@ -603,6 +650,7 @@ def unregister():
     bpy.utils.unregister_class(OpExportAnm)
     bpy.utils.unregister_class(OpExportObject)
     bpy.types.INFO_MT_file_import.remove(menu_func_import)
+    bpy.utils.unregister_class(OpImportDetails)
     bpy.utils.unregister_class(OpImportDM)
     bpy.utils.unregister_class(OpImportSkl)
     bpy.utils.unregister_class(OpImportAnm)
