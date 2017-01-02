f785a2646d5180cfbf5e482969ed5b39de4c8fc1
Vakhurin Sergey <igelbox@gmail.com>
2017-01-03 02:22:01 +0300

    tests: import object with empty polygons

---
+++ tests/cases/test_fmt_object.py
@@ -0,0 +1,13 @@
+from tests import utils
+
+import bpy
+import re
+
+
+class TestFormatObject(utils.XRayTestCase):
+    def test_import_with_empty_polygons(self):
+        # Act
+        bpy.ops.xray_import.object(
+            directory=self.relpath(),
+            files=[{'name': 'test_fmt_invalid_face.object'}],
+        )
