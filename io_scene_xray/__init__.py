1649db45302ebc0f3faa3c19b6868e05a20c20a6
PavelBlend <stalkermodkytia@yandex.ru>
2017-01-17 11:45:26 +0300

    returned to the previous version of the format in bl_info

--- io_scene_xray/__init__.py
+++ io_scene_xray/__init__.py
@@ -1,15 +1,14 @@
-
 bl_info = {
-    'name': 'XRay Engine Tools',
-    'author': 'Vakhurin Sergey (igel)',
-    'version': (0, 2, 5),
-    'blender': (2, 7, 0),
+    'name':     'XRay Engine Tools',
+    'author':   'Vakhurin Sergey (igel)',
+    'version':  (0, 2, 5),
+    'blender':  (2, 7, 0),
     'category': 'Import-Export',
     'location': 'File > Import/Export',
-    'description': 'Import/Export X-Ray Engine formats',
+    'description': 'Import/Export X-Ray objects',
     'wiki_url': 'https://github.com/igelbox/blender-xray',
     'tracker_url': 'https://github.com/igelbox/blender-xray/issues',
-    'warning': 'Under construction!'
-    }
+    'warning':  'Under construction!'
+}
 
 from .plugin import register, unregister
