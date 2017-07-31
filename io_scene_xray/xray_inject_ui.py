2ef973b98a6e59d04fd5476d199d714c53f0bdd6
Vakhurin Sergey <igelbox@gmail.com>
2017-07-31 22:55:41 +0300

    move XRay's Action panel from GraphEditor to DopeSheetEditor

--- io_scene_xray/xray_inject_ui.py
+++ io_scene_xray/xray_inject_ui.py
@@ -388,7 +388,7 @@ class XRayBonePanel(XRayPanel):
 
 class XRayActionPanel(XRayPanel):
     bl_category = 'F-Curve'
-    bl_space_type = 'GRAPH_EDITOR'
+    bl_space_type = 'DOPESHEET_EDITOR' if bpy.app.version >= (2, 78, 0) else 'GRAPH_EDITOR'
     bl_region_type = 'UI'
     bl_context = 'object'
 
