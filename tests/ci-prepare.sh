c6aefd005174337fcbad51b5ff8f72353aa18780
Vakhurin Sergey <igelbox@gmail.com>
2017-11-20 00:48:57 +0300

    fix TravisCI builds

--- tests/ci-prepare.sh
+++ tests/ci-prepare.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 
 if [ ! -e "blender/blender" ]; then
     TBZ=blender-2.77-linux-glibc211-x86_64.tar.bz2
@@ -11,7 +12,7 @@ if [ ! -e "blender/blender" ]; then
     mkdir -p $TGT
     ln -s io_scene_xray $TGT/
 
-    wget http://pypi.python.org/packages/53/fe/9e0fbdbca15c2c1253379c3a694f4315a420555e7874445b06edeaeacaea/coverage-4.2.tar.gz#md5=1e09362a3f35d589f942359441050a6e
+    wget https://pypi.python.org/packages/53/fe/9e0fbdbca15c2c1253379c3a694f4315a420555e7874445b06edeaeacaea/coverage-4.2.tar.gz#md5=1e09362a3f35d589f942359441050a6e
     tar zxf coverage-4.2.tar.gz
     mv coverage-4.2/coverage blender/2.77/python/lib/python3.5/
     rm -rf coverage-4.2
