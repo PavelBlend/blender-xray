263d296d8aa4b8e4a07fe5ef79cbf294b4dc22f3
Vakhurin Sergey <igelbox@gmail.com>
2018-01-09 01:46:51 +0300

    add an ability to edit bone center of mass

---
+++ io_scene_xray/edit_helpers/base.py
@@ -0,0 +1,115 @@
+import bpy
+
+from io_scene_xray import registry, utils
+
+__HELPERS__ = dict()
+
+class AbstractHelper:
+    def __init__(self, name):
+        if name in __HELPERS__:
+            raise AssertionError('helper with name ' + name + ' is already registered')
+        __HELPERS__[name] = self
+        self._name = utils.HELPER_OBJECT_NAME_PREFIX + name
+
+    def get_helper(self):
+        return bpy.data.objects.get(self._name)
+
+    def get_target(self):
+        helper = self.get_helper()
+        if helper is None:
+            return
+        return helper, self._get_target_object(helper)
+
+    def activate(self, target):
+        helper = self.get_helper()
+        if helper is None:
+            helper = self._create_helper(self._name)
+            helper.draw_type = 'WIRE'
+            helper.show_x_ray = True
+            helper.hide_render = True
+            bpy.context.scene.objects.link(helper)
+
+        helper.parent = bpy.context.active_object
+        self._update_helper(helper, target)
+
+        AbstractHelper._select_object(helper)
+
+    def update(self):
+        helper = self.get_helper()
+        if helper is None:
+            return
+        target = self._get_target_object(helper)
+        self._update_helper(helper, target)
+
+    def deactivate(self):
+        helper = self.get_helper()
+        if helper is not None:
+            AbstractHelper._select_object(helper.parent)
+            self._delete_helper(helper)
+
+    def draw(self, layout, context):
+        if self.is_active(context):
+            layout.operator(EditCancel.bl_idname, icon='X')
+
+    def is_active(self, context=bpy.context):
+        obj = context.active_object
+        if utils.is_helper_object(obj):
+            target = self._get_target_object(obj)
+            return target is not None
+        target = self.get_target()
+        return target and self._is_active_target(target[1], context)
+
+    @staticmethod
+    def _select_object(_object):
+        bpy.context.scene.objects.active = _object
+        for obj in bpy.context.selectable_objects:
+            obj.select = obj == _object
+
+    def _is_active_target(self, target, context):
+        raise NotImplementedError()
+
+    def _get_target_object(self, helper):
+        raise NotImplementedError()
+
+    def _create_helper(self, name):
+        raise NotImplementedError()
+
+    def _delete_helper(self, helper):
+        bpy.data.objects.remove(helper)
+
+    def _update_helper(self, helper, target):
+        raise NotImplementedError()
+
+
+def get_object_helper(context):
+    obj = context.active_object
+    if not utils.is_helper_object(obj):
+        return None
+    name = obj.name[len(utils.HELPER_OBJECT_NAME_PREFIX):]
+    helper = __HELPERS__[name]
+    if not helper.is_active(context):
+        return None
+    return helper
+
+
+@registry.module_thing
+class EditCancel(bpy.types.Operator):
+    bl_idname = 'io_scene_xray.edit_cancel'
+    bl_label = 'Cancel Editing'
+    bl_description = 'Cancel editing and remove a helper object'
+
+    @classmethod
+    def poll(cls, context):
+        return _get_active_helper(context) is not None
+
+    def execute(self, context):
+        helper = _get_active_helper(context)
+        helper.deactivate()
+        return {'FINISHED'}
+
+
+def _get_active_helper(context):
+    for helper in __HELPERS__.values():
+        if helper.is_active(context):
+            return helper
+    return None
---
+++ io_scene_xray/edit_helpers/bone_center.py
@@ -0,0 +1,88 @@
+import bpy
+import mathutils
+
+from io_scene_xray import registry
+from io_scene_xray.xray_motions import MATRIX_BONE, MATRIX_BONE_INVERTED
+from .base import AbstractHelper
+
+
+class _BoneCenterEditHelper(AbstractHelper):
+    def draw(self, layout, context):
+        super().draw(layout, context)
+        if self.is_active(context):
+            layout.operator(_CenterEditApplyOp.bl_idname, icon='FILE_TICK')
+            return
+
+        lay = layout
+        if context.active_bone.xray.shape.type == '0':
+            lay = lay.split(align=True)
+            lay.enabled = False
+        lay.operator(EditCenter.bl_idname, text='Edit Center')
+
+    def _is_active_target(self, target, context):
+        if target is None:
+            return False
+        bone = context.active_bone
+        if bone is None:
+            return False
+        return (bone.name == target.name) and (bone.id_data == target.id_data)
+
+    def _get_target_object(self, helper):
+        split = helper.xray.helper_data.split('/')
+        if len(split) != 2:
+            return
+        arm = bpy.data.armatures.get(split[0], None)
+        if arm is None:
+            return
+        bone = arm.bones.get(split[1], None)
+        if bone is None:
+            return
+        return bone
+
+    def _create_helper(self, name):
+        helper = bpy.data.objects.new(name, None)
+        helper.empty_draw_size = 0.05
+        helper.lock_rotation = helper.lock_scale = (True, True, True)
+        return helper
+
+    def _update_helper(self, helper, target):
+        bone = target
+
+        mat = bone.matrix_local * MATRIX_BONE_INVERTED
+        mat *= mathutils.Matrix.Translation(bone.xray.mass.center)
+        helper.location = mat.to_translation()
+        helper.xray.helper_data = bone.id_data.name + '/' + bone.name
+
+
+HELPER = _BoneCenterEditHelper('bone-center-edit')
+
+
+@registry.module_thing
+class EditCenter(bpy.types.Operator):
+    bl_idname = 'io_scene_xray.edit_bone_center'
+    bl_label = 'Edit Bone Center'
+    bl_description = 'Create a helper object that can be used for adjusting bone center'
+
+    @classmethod
+    def poll(cls, context):
+        return context.active_bone and not HELPER.is_active(context)
+
+    def execute(self, context):
+        target = context.active_object.data.bones[context.active_bone.name]
+        HELPER.activate(target)
+        return {'FINISHED'}
+
+
+@registry.module_thing
+class _CenterEditApplyOp(bpy.types.Operator):
+    bl_idname = 'io_scene_xray.edit_bone_center_apply'
+    bl_label = 'Apply Center'
+    bl_options = {'UNDO'}
+
+    def execute(self, _context):
+        helper, bone = HELPER.get_target()
+        mat = MATRIX_BONE * bone.matrix_local.inverted() * helper.matrix_local
+        bone.xray.mass.center = mat.to_translation()
+        HELPER.deactivate()
+        bpy.context.scene.update()
+        return {'FINISHED'}
