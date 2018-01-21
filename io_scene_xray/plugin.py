5e80a0c6eb5b1ba60b4655dcf829687484cf2922
Vakhurin Sergey <igelbox@gmail.com>
2018-01-21 19:58:29 +0300

    import skls: add 'select/deselect all' and 'deselect existing' operators

--- io_scene_xray/plugin.py
+++ io_scene_xray/plugin.py
@@ -1,4 +1,5 @@
 import os.path
+import re
 
 import bpy
 from bpy_extras import io_utils
@@ -7,6 +8,7 @@ from . import xray_inject
 from .ops import BaseOperator as TestReadyOperator
 from .ui import collapsible
 from .utils import AppError, ObjectsInitializer, logger
+from .xray_motions import MOTIONS_FILTER_ALL
 from . import plugin_prefs
 from . import registry
 
@@ -135,9 +137,11 @@ def invoke_require_armature(func):
     return wrapper
 
 
+__MOTIONS_LIST__ = [None]
 @registry.module_thing
 class _MotionsList(bpy.types.UIList):
     def draw_item(self, _context, layout, _data, item, _icon, _active_data, _active_propname):
+        __MOTIONS_LIST__[0] = self  # A dirty hack
         row = layout.row(align=True)
         row.prop(
             item, 'flag',
@@ -146,12 +150,56 @@ class _MotionsList(bpy.types.UIList):
         )
         row.label(item.name)
 
+class _BaseSelectMotionsOp(bpy.types.Operator):
+    def execute(self, context):
+        data = getattr(context, _DeselectMotionsOp.bl_idname + '.data')
+        mlist = __MOTIONS_LIST__[0]
+        name_filter = MOTIONS_FILTER_ALL
+        if mlist and mlist.filter_name:
+            rgx = re.compile('.*' + re.escape(mlist.filter_name).replace('\\*', '.*') + '.*')
+            name_filter = lambda name: (rgx.match(name) is not None) ^ mlist.use_filter_invert
+        for motion in data.motions:
+            if name_filter(motion.name):
+                self._update_motion(motion)
+        return {'FINISHED'}
+
+    def _update_motion(self, motion):
+        pass
+
+@registry.module_thing
+class _SelectMotionsOp(_BaseSelectMotionsOp):
+    bl_idname = 'io_scene_xray.motions_select'
+    bl_label = 'Select'
+    bl_description = 'Select all displayed importing motions'
+
+    def _update_motion(self, motion):
+        motion.flag = True
+
+@registry.module_thing
+class _DeselectMotionsOp(_BaseSelectMotionsOp):
+    bl_idname = 'io_scene_xray.motions_deselect'
+    bl_label = 'Deselect'
+    bl_description = 'Deselect all displayed importing motions'
+
+    def _update_motion(self, motion):
+        motion.flag = False
+
+@registry.module_thing
+class _DeselectDuplicatedMotionsOp(_BaseSelectMotionsOp):
+    bl_idname = 'io_scene_xray.motions_deselect_duplicated'
+    bl_label = 'Dups'
+    bl_description = 'Deselect displayed importing motions which already exist in the scene'
+
+    def _update_motion(self, motion):
+        if bpy.data.actions.get(motion.name):
+            motion.flag = False
+
 
 @registry.module_thing
 @registry.requires('Motion')
 class OpImportSkl(TestReadyOperator, io_utils.ImportHelper):
     class Motion(bpy.types.PropertyGroup):
-        flag = bpy.props.BoolProperty(name='Selected for Import')
+        flag = bpy.props.BoolProperty(name='Selected for Import', default=True)
         name = bpy.props.StringProperty(name='Motion Name')
 
     bl_idname = 'xray_import.skl'
@@ -164,52 +212,59 @@ class OpImportSkl(TestReadyOperator, io_utils.ImportHelper):
     directory = bpy.props.StringProperty(subtype='DIR_PATH')
     files = bpy.props.CollectionProperty(type=bpy.types.OperatorFileListElement)
 
-    motions_preview_items = bpy.props.CollectionProperty(type=Motion)
-    motions_preview_index = bpy.props.IntProperty()
+    motions = bpy.props.CollectionProperty(type=Motion, name='Motions Filter')
     __parsed_file_name = None
 
-    def draw(self, _context):
+    def draw(self, context):
         layout = self.layout
         row = layout.row()
         row.enabled = False
         row.label('%d items' % len(self.files))
 
-        key = self.bl_idname
-        text = 'Filter Motions: '
-        if collapsible.is_opened(key):
-            count = len([m for m in self.motions_preview_items if m.flag])
-            text += str(count)
-        else:
-            text += 'All'
-
+        motions, count = self._get_motions(), 0
+        text = 'Filter Motions'
+        enabled = len(motions) > 1
+        if enabled:
+            text = 'Filter Motions: '
+            count = len([m for m in motions if m.flag])
+            if count == len(motions):
+                text += 'All (%d)' % count
+            else:
+                text += str(count)
         _, box = collapsible.draw(
             layout,
             self.bl_idname,
             text,
-            enabled=len(self._get_motions()) > 1,
-            icon='FILTER',
+            enabled=enabled,
+            icon='FILTER' if count < 100 else 'ERROR',
             style='nobox',
         )
         if box:
-            box.template_list(
-                '_MotionsList', 'name',
-                self, 'motions_preview_items',
-                self, 'motions_preview_index',
+            col = box.column(align=True)
+            __MOTIONS_LIST__[0] = None
+            col.template_list(
+                '_MotionsList', '',
+                self, 'motions',
+                context.scene.xray.import_skls, 'motion_index',
             )
+            row = col.row(align=True)
+            row.context_pointer_set(_DeselectMotionsOp.bl_idname + '.data', self)
+            row.operator(_SelectMotionsOp.bl_idname, icon='CHECKBOX_HLT')
+            row.operator(_DeselectMotionsOp.bl_idname, icon='CHECKBOX_DEHLT')
+            row.operator(_DeselectDuplicatedMotionsOp.bl_idname, icon='COPY_ID')
 
     def _get_motions(self):
+        items = self.motions
         if len(self.files) == 1:
             fpath = os.path.join(self.directory, self.files[0].name)
             if self.__parsed_file_name != fpath:
-                items = self.motions_preview_items
                 items.clear()
                 for name in self._examine_file(fpath):
                     items.add().name = name
-            self.__parsed_file_name = fpath
+                self.__parsed_file_name = fpath
         else:
-            self.motions_preview_items.clear()
-
-        return self.motions_preview_items
+            items.clear()
+        return items
 
     @staticmethod
     def _examine_file(fpath):
@@ -225,9 +280,9 @@ class OpImportSkl(TestReadyOperator, io_utils.ImportHelper):
             self.report({'ERROR'}, 'No files selected')
             return {'CANCELLED'}
         from .fmt_skl_imp import import_skl_file, import_skls_file, ImportContext
-        motions_filter = lambda name: True
-        if self.motions_preview_items:
-            selected_names = set(m.name for m in self.motions_preview_items if m.flag)
+        motions_filter = MOTIONS_FILTER_ALL
+        if self.motions:
+            selected_names = set(m.name for m in self.motions if m.flag)
             motions_filter = lambda name: name in selected_names
         import_context = ImportContext(
             armature=context.active_object,
