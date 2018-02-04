19abbdb79cf80612e38691f8682cd0cc23387560
PavelBlend <stalkermodkytia@yandex.ru>
2018-02-04 20:32:41 +0300

    test err: correct filepath

--- tests/cases/test_err.py
+++ tests/cases/test_err.py
@@ -1,16 +1,12 @@
-import os
+import bpy
 
 from tests import utils
-
-import bpy
 
 
 class TestErrImport(utils.XRayTestCase):
     def test_default(self):
         # Act
-        bpy.ops.xray_import.err(
-            filepath=self.relpath() + os.sep + 'test_fmt.err'
-        )
+        bpy.ops.xray_import.err(filepath=self.relpath('test_fmt.err'))
 
         # Assert
         self.assertReportsNotContains('WARNING')
