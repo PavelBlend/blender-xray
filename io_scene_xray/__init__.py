e05b75245f7b1ca6dfe3e2e57603a1234065792c
PavelBlend <stalkermodkytia@yandex.ru>
2018-10-17 12:06:19 +0400

    __init__.py: PEP8 refactor

--- io_scene_xray/__init__.py
+++ io_scene_xray/__init__.py
@@ -1,14 +1,14 @@
 bl_info = {
-    'name':     'XRay Engine Tools',
-    'author':   'Vakhurin Sergey (igel), Pavel_Blend',
-    'version':  (0, 5, 0),
-    'blender':  (2, 77, 0),
+    'name': 'XRay Engine Tools',
+    'author': 'Vakhurin Sergey (igel), Pavel_Blend',
+    'version': (0, 5, 0),
+    'blender': (2, 77, 0),
     'category': 'Import-Export',
     'location': 'File > Import/Export',
     'description': 'Import/Export X-Ray objects',
     'wiki_url': 'https://github.com/PavelBlend/blender-xray',
     'tracker_url': 'https://github.com/PavelBlend/blender-xray/issues',
-    'warning':  'Under construction!'
+    'warning': 'Under construction!'
 }
 
 
