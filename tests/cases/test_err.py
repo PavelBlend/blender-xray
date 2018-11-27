7db3374220fe8d733a03b89adbfb5ddc21ba2aef
PavelBlend <stalkermodkytia@yandex.ru>
2018-11-27 13:17:40 +0300

    add test for sdk *.err format

--- tests/cases/test_err.py
+++ tests/cases/test_err.py
@@ -13,3 +13,13 @@ class TestErrImport(utils.XRayTestCase):
 
         obj = bpy.data.objects['test_fmt.err']
         self.assertNotEqual(len(obj.data.polygons), 0)
+
+    def test_sdk_err_format(self):
+        # Act
+        bpy.ops.xray_import.err(filepath=self.relpath('test_fmt_sdk.err'))
+
+        # Assert
+        self.assertReportsNotContains('WARNING')
+
+        obj = bpy.data.objects['test_fmt_sdk.err']
+        self.assertNotEqual(len(obj.data.polygons), 0)
