26866d9d2afa5637fd4f1a77dd112c831dc92e3f
PavelBlend <stalkermodkytia@yandex.ru>
2019-02-14 22:48:53 +0300

    joint limits: properties limit

--- io_scene_xray/xray_inject.py
+++ io_scene_xray/xray_inject.py
@@ -350,18 +350,18 @@ class XRayBoneProperties(bpy.types.PropertyGroup):
             ('4', 'None', ''),
             ('5', 'Slider', '')))
 
-        lim_x_min = bpy.props.FloatProperty()
-        lim_x_max = bpy.props.FloatProperty()
+        lim_x_min = bpy.props.FloatProperty(min=-180.0, max=180)
+        lim_x_max = bpy.props.FloatProperty(min=-180.0, max=180)
         lim_x_spr = bpy.props.FloatProperty()
         lim_x_dmp = bpy.props.FloatProperty()
 
-        lim_y_min = bpy.props.FloatProperty()
-        lim_y_max = bpy.props.FloatProperty()
+        lim_y_min = bpy.props.FloatProperty(min=-180.0, max=180)
+        lim_y_max = bpy.props.FloatProperty(min=-180.0, max=180)
         lim_y_spr = bpy.props.FloatProperty()
         lim_y_dmp = bpy.props.FloatProperty()
 
-        lim_z_min = bpy.props.FloatProperty()
-        lim_z_max = bpy.props.FloatProperty()
+        lim_z_min = bpy.props.FloatProperty(min=-180.0, max=180)
+        lim_z_max = bpy.props.FloatProperty(min=-180.0, max=180)
         lim_z_spr = bpy.props.FloatProperty()
         lim_z_dmp = bpy.props.FloatProperty()
 
