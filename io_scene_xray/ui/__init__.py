0c5261be3cd25dba732f05d7bce628314273c96e
PavelBlend <stalkermodkytia@yandex.ru>
2019-06-22 01:43:26 +0400

    support blender 2.80: fix preferences

--- io_scene_xray/ui/__init__.py
+++ io_scene_xray/ui/__init__.py
@@ -1,11 +1,17 @@
-from ..version_utils import layout_split
+from ..version_utils import layout_split, IS_28
 
 
 def xprop(layout, data, prop, enabled=True, **kwargs):
-    attrs = getattr(data.bl_rna, prop)[1]
-    name = attrs.get('name', prop)
+    if IS_28:
+        attrs = data.bl_rna.properties.get(prop)
+        name = attrs.name
+        split_factor = 0.242
+    else:
+        attrs = getattr(data.bl_rna, prop)[1]
+        name = attrs.get('name', prop)
+        split_factor = 0.33
     row = layout.row()
-    lay = layout_split(row, 0.33)
+    lay = layout_split(row, split_factor)
     lay.label(text=name + ':')
     lay = lay.row(align=True)
     lay_l = lay.row(align=True)
