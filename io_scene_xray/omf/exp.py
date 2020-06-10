2aa595579960ad6d93c8f368c1f98fc9feb29c19
PavelBlend <stalkermodkytia@yandex.ru>
2020-06-10 08:54:45 +0300

    fix omf export

--- io_scene_xray/omf/exp.py
+++ io_scene_xray/omf/exp.py
@@ -44,10 +44,8 @@ def export_omf_file(filepath, bpy_obj):
         bpy_obj.animation_data_create()
         bpy_obj.animation_data.action = action
         packed_writer.puts(name)
-        length = int(action.frame_range[1] - action.frame_range[0])
+        length = int(action.frame_range[1] - action.frame_range[0] + 1)
         packed_writer.putf('I', length)
-        flags = fmt.FL_T_KEY_PRESENT
-        packed_writer.putf('B', flags)
         bone_matrices = {}
         bone_parent_matrices = {}
         for frame_index in range(int(action.frame_range[0]), int(action.frame_range[1]) + 1):
@@ -62,6 +60,8 @@ def export_omf_file(filepath, bpy_obj):
                 else:
                     bone_parent_matrices[pose_bone.name].append(imp.MATRIX_BONE)
         for pose_bone in pose_bones:
+            flags = fmt.FL_T_KEY_PRESENT
+            packed_writer.putf('B', flags)
             motion_crc32 = 0x0    # temp value
             packed_writer.putf('I', motion_crc32)
             for matrix_index, matrix in enumerate(bone_matrices[pose_bone.name]):
@@ -73,7 +73,6 @@ def export_omf_file(filepath, bpy_obj):
                 # translation
                 packed_writer.putf('3b', 0, 0, 0)    # temp values
             packed_writer.putf('3f', 1, 1, 1)    # t_size
-            packed_writer.putf('3f', 0, 0, 0)    # t_init
             packed_writer.putf('3f', 0, 0, 0)    # t_init
         chunked_writer.put(chunk_id, packed_writer)
         chunk_id += 1
