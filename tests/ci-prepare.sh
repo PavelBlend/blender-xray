06f8686009b22ec2bf44432aff655110e464f9d6
PavelBlend <stalkermodkytia@yandex.ru>
2020-06-19 18:43:39 +0300

    fix tests for 2.83

--- tests/ci-prepare.sh
+++ tests/ci-prepare.sh
@@ -30,7 +30,7 @@ if [ ! -e "$DIR/blender" ]; then
             PYTHON_VERSION="3.7"
             ;;
         "2.83")
-            TAR="blender-$BLENDER_VERSION-linux64.tar.xz"
+            TAR="blender-$BLENDER_VERSION.0-linux64.tar.xz"
             PYTHON_VERSION="3.7"
             ;;
         "2.90")
