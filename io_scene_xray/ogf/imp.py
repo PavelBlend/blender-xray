e0a9e63f23e8634ada5f01a6e919c4c2fc8f7b71
PavelBlend <stalkermodkytia@yandex.ru>
2020-07-03 12:46:27 +0300

    level normals: change normal type

--- io_scene_xray/ogf/imp.py
+++ io_scene_xray/ogf/imp.py
@@ -52,7 +52,7 @@ def convert_normal(norm_in):
     norm_out_x = 2.0 * norm_in[0] / 255 - 1.0
     norm_out_y = 2.0 * norm_in[1] / 255 - 1.0
     norm_out_z = 2.0 * norm_in[2] / 255 - 1.0
-    return mathutils.Vector((norm_out_z, norm_out_x, norm_out_y))
+    return (norm_out_z, norm_out_x, norm_out_y)
 
 
 def create_visual(bpy_mesh, visual, level, geometry_key):
