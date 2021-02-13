7906d1c3b34540a1f749aac3d9babb72d34587c6
PavelBlend <stalkermodkytia@yandex.ru>
2021-02-13 17:01:46 +0300

    *.bones format: tests for export

--- tests/cases/test_bones_import.py
+++ tests/cases/test_bones_import.py
@@ -13,7 +13,11 @@ class TestBonesImport(utils.XRayTestCase):
         arm_obj = bpy.data.objects['test_fmt_bones.object']
         utils.set_active_object(arm_obj)
 
-        # Act
+        # remove "bone or parts" (bone groups)
+        for bone_group in arm_obj.pose.bone_groups:
+            arm_obj.pose.bone_groups.remove(bone_group)
+
+        # Act import
         bpy.ops.xray_import.bones(
             directory=self.relpath(),
             files=[{'name': 'test_fmt.bones'}],
@@ -23,3 +27,41 @@ class TestBonesImport(utils.XRayTestCase):
 
         # Assert
         self.assertReportsNotContains('WARNING')
+
+        # Act export
+        bpy.ops.xray_export.bones(
+            directory=self.relpath(),
+            filepath=self.outpath('test.bones'),
+            object_name=arm_obj.name,
+            export_bone_parts=True,
+            export_bone_properties=True
+        )
+
+        # test blender ik limits
+        arm_obj.data.xray.joint_limits_type = 'IK'
+        bpy.ops.xray_export.bones(
+            directory=self.relpath(),
+            filepath=self.outpath('test_blender_ik.bones'),
+            object_name=arm_obj.name,
+            export_bone_parts=True,
+            export_bone_properties=True
+        )
+
+        # test export null boneparts
+        # remove "bone or parts" (bone groups)
+        for bone_group in arm_obj.pose.bone_groups:
+            arm_obj.pose.bone_groups.remove(bone_group)
+
+        # Act export
+        bpy.ops.xray_export.bones(
+            directory=self.relpath(),
+            filepath=self.outpath('test_nul_boneparts.bones'),
+            object_name=arm_obj.name,
+            export_bone_parts=True,
+            export_bone_properties=True
+        )
+
+        # Assert
+        self.assertOutputFiles({
+            'test.bones', 'test_blender_ik.bones', 'test_nul_boneparts.bones'
+        })
