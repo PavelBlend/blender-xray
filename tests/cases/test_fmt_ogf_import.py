9d6a33ce36c932c4cef21fcae3fdffaeffb0b2c2
PavelBlend <stalkermodkytia@yandex.ru>
2021-09-06 19:24:38 +0300

    import ogf: added tests

---
+++ tests/cases/test_fmt_ogf_import.py
@@ -0,0 +1,27 @@
+from tests import utils
+
+import bpy
+
+
+class TestOgf(utils.XRayTestCase):
+    def test_import_general(self):
+        bpy.ops.xray_import.ogf(
+            directory=self.relpath(),
+            files=[{'name': 'test_fmt_ogf_st.ogf'}],
+        )
+        bpy.ops.xray_import.ogf(
+            directory=self.relpath(),
+            files=[{'name': 'test_fmt_ogf_pm_1_link.ogf'}],
+        )
+        bpy.ops.xray_import.ogf(
+            directory=self.relpath(),
+            files=[{'name': 'test_fmt_ogf_pm_act.ogf'}],
+        )
+        bpy.ops.xray_import.ogf(
+            directory=self.relpath(),
+            files=[
+                {'name': 'test_fmt_ogf_pm_act.ogf'},
+                {'name': 'test_fmt_ogf_pm_1_link.ogf'},
+                {'name': 'test_fmt_ogf_st.ogf'}
+            ],
+        )
