c03ec369abf72f6fbf0771e8c66a3fecd78a0e1b
PavelBlend <stalkermodkytia@yandex.ru>
2021-09-24 13:38:18 +0300

    viewer: view all after import

--- io_scene_xray/viewer.py
+++ io_scene_xray/viewer.py
@@ -125,6 +125,12 @@ def update_file(self, context):
     new_objects = get_current_objects()
     imported_objects = old_objects ^ new_objects
     scene['imported_objects'] = list(imported_objects)
+    for area in bpy.context.screen.areas:
+        if area.type == 'VIEW_3D':
+            ctx = bpy.context.copy()
+            ctx['area'] = area
+            ctx['region'] = area.regions[-1]
+            bpy.ops.view3d.view_all(ctx, center=False) 
 
 
 ext_list = ['.ogf', '.object', '.dm', '.details']
