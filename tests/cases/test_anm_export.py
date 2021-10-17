0edf1e4c7828c737b88cfcdb05d0b3cece8d95c4
PavelBlend <stalkermodkytia@yandex.ru>
2021-10-17 19:54:40 +0300

    improved anm export tests

--- tests/cases/test_anm_export.py
+++ tests/cases/test_anm_export.py
@@ -1,6 +1,8 @@
-from tests import utils
+import re
 
 import bpy
+
+from tests import utils
 
 
 class TestAnmExport(utils.XRayTestCase):
@@ -17,6 +19,120 @@ class TestAnmExport(utils.XRayTestCase):
         self.assertOutputFiles({
             'Cube1.anm'
         })
+
+
+    def test_has_no_rot(self):
+        # Arrange
+        obj = self._create_active_object()
+        obj.rotation_mode = 'YXZ'
+        act = bpy.data.actions.new('tact')
+        for i in range(3):
+            fcu = act.fcurves.new('location', index=i)
+            fcu.keyframe_points.insert(1, 0)
+
+        obj.animation_data_create().action = act
+
+        # Act
+        bpy.ops.xray_export.anm(
+            filepath=self.outpath('test.anm'),
+        )
+        self.assertReportsContains('ERROR', re.compile('Action has keys not for all channels'))
+
+    def test_has_no_loc(self):
+        # Arrange
+        obj = self._create_active_object()
+        obj.rotation_mode = 'YXZ'
+        act = bpy.data.actions.new('tact')
+        for i in range(3):
+            fcu = act.fcurves.new('rotation_euler', index=i)
+            fcu.keyframe_points.insert(1, 0)
+
+        obj.animation_data_create().action = act
+
+        # Act
+        bpy.ops.xray_export.anm(
+            filepath=self.outpath('test.anm'),
+        )
+        self.assertReportsContains('ERROR', re.compile('Action has keys not for all channels'))
+
+    def test_has_no_loc_rot(self):
+        # Arrange
+        obj = self._create_active_object()
+        obj.rotation_mode = 'YXZ'
+        act = bpy.data.actions.new('tact')
+        obj.animation_data_create().action = act
+
+        # Act
+        bpy.ops.xray_export.anm(
+            filepath=self.outpath('test.anm'),
+        )
+        self.assertReportsContains('ERROR', re.compile('Action has keys not for all channels'))
+
+    def test_custom_refine(self):
+        # Arrange
+        obj = self._create_active_object()
+        obj.rotation_mode = 'YXZ'
+        act = bpy.data.actions.new('tact')
+        act.xray.autobake_custom_refine = True
+        obj.animation_data_create().action = act
+
+        for i in range(3):
+            fcu = act.fcurves.new('location', index=i)
+            fcu.keyframe_points.insert(1, 0)
+            fcu.keyframe_points.insert(10, 1)
+            fcu = act.fcurves.new('rotation_euler', index=i)
+            fcu.keyframe_points.insert(1, 0)
+            fcu.keyframe_points.insert(10, 1)
+
+        # Act
+        bpy.ops.xray_export.anm(
+            filepath=self.outpath('test.anm'),
+        )
+
+    def test_bake_on(self):
+        # Arrange
+        obj = self._create_active_object()
+        obj.rotation_mode = 'YXZ'
+        act = bpy.data.actions.new('tact')
+        act.xray.autobake_custom_refine = True
+        act.xray.autobake_on = True
+        obj.animation_data_create().action = act
+
+        for i in range(3):
+            fcu = act.fcurves.new('location', index=i)
+            fcu.keyframe_points.insert(1, 0)
+            fcu.keyframe_points.insert(10, 1)
+            fcu = act.fcurves.new('rotation_euler', index=i)
+            fcu.keyframe_points.insert(1, 0)
+            fcu.keyframe_points.insert(10, 1)
+
+        # Act
+        bpy.ops.xray_export.anm(
+            filepath=self.outpath('test.anm'),
+        )
+
+    def test_color_keys(self):
+        # Arrange
+        obj = self._create_active_object()
+        obj.rotation_mode = 'YXZ'
+        act = bpy.data.actions.new('tact')
+        obj.animation_data_create().action = act
+
+        for i in range(3):
+            fcu = act.fcurves.new('location', index=i)
+            fcu.keyframe_points.insert(1, 0)
+            fcu.keyframe_points.insert(10, 1)
+            fcu = act.fcurves.new('rotation_euler', index=i)
+            fcu.keyframe_points.insert(1, 0)
+            fcu.keyframe_points.insert(10, 1)
+            fcu = act.fcurves.new('color', index=i)
+            fcu.keyframe_points.insert(1, 0)
+            fcu.keyframe_points.insert(10, 1)
+
+        # Act
+        bpy.ops.xray_export.anm(
+            filepath=self.outpath('test.anm'),
+        )
 
     def test_ok(self):
         # Arrange
