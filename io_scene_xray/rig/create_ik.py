83da3baa8ba22ca5184e60d2ad68821295f096c3
Pavel_Blend <stalkermodkytia@yandex.ru>
2021-12-11 17:40:43 +0300

    create ik: change custom props settings and added rig panel

--- io_scene_xray/rig/create_ik.py
+++ io_scene_xray/rig/create_ik.py
@@ -10,28 +10,25 @@ from .. import version_utils
 from .. import text
 
 
-props = {
-    'chain_length': bpy.props.IntProperty(
-        name='Chain Length',
-        min=0,
-        max=255,
-        default=2
-    ),
-    'pole_target_offset': bpy.props.FloatProperty(
-        name='Pole Target Offset',
-        min=0.001,
-        default=0.5
-    ),
-}
 bone_layers = [False, ] * 32
 last_layer = bone_layers.copy()
 last_layer[31] = True
 layer_30 = bone_layers.copy()
 layer_30[30] = True
 IK_FK_PROP_NAME = 'ik_fk'
+ik_suffix = ' ik'
+fk_suffix = ' fk'
+ik_target_suffix = ' ik_target'
+ik_pole_target_suffix = ' ik_pole_target'
+bone_suffix_list = (
+    ik_suffix,
+    fk_suffix,
+    ik_target_suffix,
+    ik_pole_target_suffix
+)
 
 
-def create_ik(bone, chain_length, pole_target_offset):
+def create_ik(bone, chain_length, pole_target_offset, category_name):
     bone_name = bone.name
     bone_root_name = bone.parent.name
     bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
@@ -47,14 +44,14 @@ def create_ik(bone, chain_length, pole_target_offset):
     for chain_index in range(chain_length):
         current_bone.layers = last_layer
         # ik bone
-        ik_bone = arm.edit_bones.new(current_bone.name + ' ik')
+        ik_bone = arm.edit_bones.new(current_bone.name + ik_suffix)
         ik_bones[current_bone.name] = ik_bone.name
         ik_bone.head = current_bone.head
         ik_bone.tail = current_bone.tail
         ik_bone.roll = current_bone.roll
         ik_bone.layers = layer_30
         # fk bone
-        fk_bone = arm.edit_bones.new(current_bone.name + ' fk')
+        fk_bone = arm.edit_bones.new(current_bone.name + fk_suffix)
         fk_bones[current_bone.name] = fk_bone.name
         fk_bone.head = current_bone.head
         fk_bone.tail = current_bone.tail
@@ -83,7 +80,7 @@ def create_ik(bone, chain_length, pole_target_offset):
                 fk_bone.parent = fk_parent
             else:
                 fk_bone.parent = parent
-    target_bone = arm.edit_bones.new(bone.name + ' ik target')
+    target_bone = arm.edit_bones.new(bone.name + ik_target_suffix)
     target_bone_name = target_bone.name
     target_bone.head = bone.tail
     tail = target_bone.head.copy()
@@ -101,7 +98,7 @@ def create_ik(bone, chain_length, pole_target_offset):
         child_edit_bone = arm.edit_bones[child_bone_name]
         child_edit_bone.layers = last_layer
         # create ik bone
-        child_ik_bone = arm.edit_bones.new(child_bone_name + ' ik')
+        child_ik_bone = arm.edit_bones.new(child_bone_name + ik_suffix)
         child_ik_bone.head = child_edit_bone.head
         child_ik_bone.tail = child_edit_bone.tail
         child_ik_bone.roll = child_edit_bone.roll
@@ -109,7 +106,7 @@ def create_ik(bone, chain_length, pole_target_offset):
         child_ik_bone.layers = last_layer
         ik_subtarget_name = child_ik_bone.name
         # create fk bone
-        child_fk_bone = arm.edit_bones.new(child_bone_name + ' fk')
+        child_fk_bone = arm.edit_bones.new(child_bone_name + fk_suffix)
         child_fk_bone.head = child_edit_bone.head
         child_fk_bone.tail = child_edit_bone.tail
         child_fk_bone.roll = child_edit_bone.roll
@@ -194,7 +191,7 @@ def create_ik(bone, chain_length, pole_target_offset):
     arrow_vec *= pole_target_offset
     final_vec = arrow_vec + B
     # Add pole target bone and place it in the scene pointed to Z+
-    pole_name = bone_name + ' pole_target'
+    pole_name = bone_name + ik_pole_target_suffix
     pole_edit_bone = arm.edit_bones.new(pole_name)
     pole_edit_bone.head = final_vec
     pole_tail_offset = mathutils.Vector((0.0, 0.1, 0.0))
@@ -258,6 +255,14 @@ def create_ik(bone, chain_length, pole_target_offset):
     ik_constr.chain_count = chain_length
     # add drivers
     obj.pose.bones[target_bone_name][IK_FK_PROP_NAME] = 1.0
+    obj.pose.bones[target_bone_name]['bone_category'] = category_name
+    ui_prop = obj.pose.bones[target_bone_name].id_properties_ui(IK_FK_PROP_NAME)
+    ui_prop.update(
+        min=0,
+        max=1,
+        soft_min=0,
+        soft_max=1
+    )
     for ik_fk_bone_name in ik_fk_bones:
         ik_fk_bone = obj.pose.bones[ik_fk_bone_name]
         # ik driver
@@ -290,6 +295,25 @@ def create_ik(bone, chain_length, pole_target_offset):
         )
 
 
+props = {
+    'chain_length': bpy.props.IntProperty(
+        name='Chain Length',
+        min=0,
+        max=255,
+        default=2
+    ),
+    'pole_target_offset': bpy.props.FloatProperty(
+        name='Pole Target Offset',
+        min=0.001,
+        default=0.5
+    ),
+    'ik_fk_name': bpy.props.StringProperty(
+        name='IK/FK Name',
+        default='IK/FK Name'
+    ),
+}
+
+
 class XRAY_OT_create_ik(bpy.types.Operator):
     bl_idname = 'io_scene_xray.create_ik'
     bl_label = 'Create IK'
@@ -318,12 +342,21 @@ class XRAY_OT_create_ik(bpy.types.Operator):
         split.label(text='Pole Target Offset:')
         split.prop(self, 'pole_target_offset', text='')
 
+        split = version_utils.layout_split(self.layout, 0.35)
+        split.label(text='IK/FK Name:')
+        split.prop(self, 'ik_fk_name', text='')
+
     def execute(self, context):
         if not len(context.selected_pose_bones):
             self.report({'WARNING'}, text.warn.ik_no_selected_bones)
             return {'FINISHED'}
         for bone in context.selected_pose_bones:
-            create_ik(bone, self.chain_length, self.pole_target_offset)
+            create_ik(
+                bone,
+                self.chain_length,
+                self.pole_target_offset,
+                self.ik_fk_name
+            )
         return {'FINISHED'}
 
     def invoke(self, context, event):
