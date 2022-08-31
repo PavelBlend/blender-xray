0e4550812abaa9324edd44e885a7ba6370bdcaa9
Pavel_Blend <stalkermodkytia@yandex.ru>
2022-08-31 18:29:29 +0300

    resolved #485: set clip end value in test operator

--- io_scene_xray/tests.py
+++ io_scene_xray/tests.py
@@ -5,6 +5,7 @@ import time
 # blender modules
 import bpy
 import bpy_extras
+import mathutils
 
 # addon modules
 from . import utils
@@ -82,6 +83,31 @@ class XRAY_OT_test_ogf_import_modal(bpy.types.Operator):
                     ctx['area'] = area
                     ctx['region'] = area.regions[-1]
                     bpy.ops.view3d.view_all(ctx, center=False)
+                    bbox_min_x = 1000000.0
+                    bbox_min_y = 1000000.0
+                    bbox_min_z = 1000000.0
+                    bbox_max_x = -1000000.0
+                    bbox_max_y = -1000000.0
+                    bbox_max_z = -1000000.0
+                    for obj in bpy.data.objects:
+                        min_bbox = mathutils.Vector(obj.bound_box[0])
+                        max_bbox = mathutils.Vector(obj.bound_box[6])
+                        bbox_min_x = min(bbox_min_x, min_bbox.x)
+                        bbox_min_y = min(bbox_min_y, min_bbox.y)
+                        bbox_min_z = min(bbox_min_z, min_bbox.z)
+                        bbox_max_x = max(bbox_max_x, min_bbox.x)
+                        bbox_max_y = max(bbox_max_y, min_bbox.y)
+                        bbox_max_z = max(bbox_max_z, min_bbox.z)
+                    dim_all_x = bbox_max_x - bbox_min_x
+                    dim_all_y = bbox_max_y - bbox_min_y
+                    dim_all_z = bbox_max_z - bbox_min_z
+                    dim_max = max(dim_all_x, dim_all_y, dim_all_z)
+                    clip_end = max(500.0, dim_max * 10)
+                    for space in area.spaces:
+                        if space.type == 'VIEW_3D':
+                            if self.clip_end is None:
+                                self.clip_end = space.clip_end
+                            space.clip_end = clip_end
         except Exception as err:
             err_text = str(err)
             if not 'Unsupported ogf format version' in err_text:
@@ -110,6 +136,13 @@ class XRAY_OT_test_ogf_import_modal(bpy.types.Operator):
                     file.write(message + '\n')
             self.log_ver.clear()
 
+    def set_clip_end(self):
+        for area in bpy.context.screen.areas:
+            if area.type == 'VIEW_3D':
+                for space in area.spaces:
+                    if space.type == 'VIEW_3D':
+                        space.clip_end = self.clip_end
+
     def modal(self, context, event):
         if event.type == 'TIMER':
             if self.last_time + self.pause < time.time():
@@ -118,15 +151,18 @@ class XRAY_OT_test_ogf_import_modal(bpy.types.Operator):
                 else:
                     context.window_manager.event_timer_remove(self.timer)
                     remove_bpy_data()
+                    self.set_clip_end()
                     return {'FINISHED'}
                 self.last_time = time.time()
         elif event.type == 'ESC':
             remove_bpy_data()
+            self.set_clip_end()
             return {'CANCELLED'}
         return {'RUNNING_MODAL'}
 
     def execute(self, context):
         self.last_time = time.time()
+        self.clip_end = None
         self.collect_files(context)
         self.timer = context.window_manager.event_timer_add(
             self.pause,
