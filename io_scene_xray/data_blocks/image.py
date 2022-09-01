29cf84fb2404e543544d7a56add7e7b377349eb4
Pavel_Blend <stalkermodkytia@yandex.ru>
2022-09-01 20:23:37 +0300

    refactor io_scene_xray\data_blocks\image module

--- io_scene_xray/data_blocks/image.py
+++ io_scene_xray/data_blocks/image.py
@@ -9,21 +9,29 @@ from .. import log
 from .. import text
 
 
-def make_relative_texture_path(a_tx_fpath, a_tx_folder):
-    a_tx_fpath = a_tx_fpath[len(a_tx_folder):].replace(os.path.sep, '\\')
-    if a_tx_fpath.startswith('\\'):
-        a_tx_fpath = a_tx_fpath[1:]
-    return a_tx_fpath
+def make_relative_texture_path(texture_file_path, textures_folder):
+    texture_file_path = texture_file_path[len(textures_folder) : ]
+    texture_file_path = texture_file_path.replace(os.path.sep, '\\')
+    if texture_file_path.startswith('\\'):
+        texture_file_path = texture_file_path[1 : ]
+    return texture_file_path
 
 
-def gen_texture_name(image, tx_folder, level_folder=None, errors=set()):
+def gen_texture_name(image, textures_folder, level_folder=None, errors=set()):
     file_path = image.filepath
-    a_tx_fpath = os.path.normpath(bpy.path.abspath(file_path))
-    a_tx_folder = os.path.abspath(tx_folder)
-    a_tx_fpath = os.path.splitext(a_tx_fpath)[0]
-    if not level_folder:    # find texture in gamedata\textures folder
-        if not a_tx_fpath.startswith(a_tx_folder):
-            drive, path_part_1 = os.path.splitdrive(a_tx_fpath)
+    tex_path = bpy.path.abspath(file_path)
+    tex_path = os.path.normpath(tex_path)
+    tex_path = os.path.splitext(tex_path)[0]
+    textures_folder = os.path.abspath(textures_folder)
+
+    # find texture in gamedata\textures folder
+    if not level_folder:
+        if tex_path.startswith(textures_folder):
+            # when the texture path is valid
+            tex_path = make_relative_texture_path(tex_path, textures_folder)
+        else:
+            # automatically fix relative path
+            drive, path_part_1 = os.path.splitdrive(tex_path)
             file_full_name = os.path.basename(path_part_1)
             file_name, ext = os.path.splitext(file_full_name)
             if path_part_1.count(os.sep) > 1:
@@ -31,32 +39,38 @@ def gen_texture_name(image, tx_folder, level_folder=None, errors=set()):
                 dir_name = os.path.basename(dir_path)
                 if file_name.startswith(dir_name + '_'):
                     relative_path = os.path.join(dir_name, file_name)
-                    a_tx_fpath = relative_path.replace(os.path.sep, '\\')
+                    tex_path = relative_path.replace(os.path.sep, '\\')
                 else:
-                    a_tx_fpath = file_name
+                    tex_path = file_name
             else:
-                a_tx_fpath = file_name
+                tex_path = file_name
             log.warn(
                 text.warn.img_bad_image_path,
                 image=image.name,
                 image_path=image.filepath,
-                textures_folder=a_tx_folder,
-                saved_as=a_tx_fpath
+                textures_folder=textures_folder,
+                saved_as=tex_path
             )
-        else:
-            a_tx_fpath = make_relative_texture_path(a_tx_fpath, a_tx_folder)
+
+    # use level folder
     else:
-        if a_tx_fpath.startswith(a_tx_folder):    # gamedata\textures folder
-            a_tx_fpath = make_relative_texture_path(a_tx_fpath, a_tx_folder)
-        elif a_tx_fpath.startswith(level_folder):    # gamedata\levels\level_name folder
-            a_tx_fpath = make_relative_texture_path(a_tx_fpath, level_folder)
-        else:    # gamedata\levels\level_name\texture_name
+        if tex_path.startswith(textures_folder):
+            # gamedata\textures folder
+            tex_path = make_relative_texture_path(tex_path, textures_folder)
+        elif tex_path.startswith(level_folder):
+            # gamedata\levels\level_name folder
+            tex_path = make_relative_texture_path(tex_path, level_folder)
+        else:
+            # gamedata\levels\level_name\texture_name
+            tex_path = os.path.split(tex_path)[-1]
             if not file_path in errors:
                 log.warn(
                     text.warn.invalid_image_path,
                     image=image.name,
-                    path=file_path
+                    path=file_path,
+                    textures_folder=textures_folder,
+                    saved_as=tex_path
                 )
                 errors.add(file_path)
-            a_tx_fpath = os.path.split(a_tx_fpath)[-1]
-    return a_tx_fpath
+
+    return tex_path
