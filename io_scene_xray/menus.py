857dd6ea1316930fb338b56a2e80d2099225e82a
Pavel_Blend <stalkermodkytia@yandex.ru>
2022-09-21 19:21:47 +0300

    refactor menus

--- io_scene_xray/menus.py
+++ io_scene_xray/menus.py
@@ -8,7 +8,7 @@ from . import formats
 
 
 # import operators: (operator, lebel)
-import_ops = [
+import_ops = (
     (formats.obj.imp.ops.XRAY_OT_import_object, 'Object'),
     (formats.skl.ops.XRAY_OT_import_skls, 'Skls'),
     (formats.ogf.ops.XRAY_OT_import_ogf, 'Ogf'),
@@ -21,10 +21,10 @@ import_ops = [
     (formats.level.ops.XRAY_OT_import_level, 'Level'),
     (formats.part.ops.XRAY_OT_import_part, 'Part'),
     (formats.err.ops.XRAY_OT_import_err, 'Err')
-]
+)
 
 # export operators: (operator, lebel)
-export_ops = [
+export_ops = (
     (formats.obj.exp.ops.XRAY_OT_export_object, 'Object'),
     (formats.skl.ops.XRAY_OT_export_skls, 'Skls'),
     (formats.skl.ops.XRAY_OT_export_skl_batch, 'Skl'),
@@ -36,97 +36,115 @@ export_ops = [
     (formats.details.ops.XRAY_OT_export_details, 'Details'),
     (formats.scene.ops.XRAY_OT_export_scene_selection, 'Scene'),
     (formats.level.ops.XRAY_OT_export_level, 'Level')
-]
+)
 
 
-def menu_func_xray_import(self, context):
+def _draw_xray_menu(self, menu):
     icon = ui.icons.get_stalker_icon()
-    self.layout.menu(XRAY_MT_import.bl_idname, icon_value=icon)
+    self.layout.menu(menu.bl_idname, icon_value=icon)
 
 
-def menu_func_xray_export(self, context):
-    icon = ui.icons.get_stalker_icon()
-    self.layout.menu(XRAY_MT_export.bl_idname, icon_value=icon)
+def _draw_xray_menu_import(self, context):
+    _draw_xray_menu(self, XRAY_MT_import)
 
 
-def append_draw_functions(ops_list, menu, mode):
-    preferences = utils.version.get_preferences()
-    for operator, label in ops_list:
-        enable_prop = 'enable_{0}_{1}'.format(label.lower(), mode)
-        enable = getattr(preferences, enable_prop)
-        if enable:
-            draw_function = utils.ie.get_draw_fun(operator)
-            menu.append(draw_function)
+def _draw_xray_menu_export(self, context):
+    _draw_xray_menu(self, XRAY_MT_export)
 
 
-def remove_draw_functions(ops_list, menu):
-    for operator, label in ops_list:
-        if hasattr(operator, 'draw_fun'):
-            draw_function = operator.draw_fun
-            menu.remove(draw_function)
-
-
-def get_enabled_operators(ops_list, mode):
+def _get_enabled_ops(ops_list, mode):
     preferences = utils.version.get_preferences()
     operators = []
     for operator, label in ops_list:
-        enable_prop = 'enable_{0}_{1}'.format(label.lower(), mode)
-        enable = getattr(preferences, enable_prop)
-        if enable:
-            id_name = operator.bl_idname
-            text = utils.draw.build_op_label(operator, compact=True)
-            operators.append((id_name, text))
+        enable_name = 'enable_{0}_{1}'.format(label.lower(), mode)
+        enable_value = getattr(preferences, enable_name)
+        if enable_value:
+            operators.append(operator)
     return operators
+
+
+def _append_standart_draw_functions(ops_list, menu, mode):
+    operators = _get_enabled_ops(ops_list, mode)
+    for operator in operators:
+        draw_function = utils.ie.get_draw_fun(operator)
+        menu.append(draw_function)
+
+
+def _append_compact_draw_functions(ops_list, menu, mode, draw):
+    operators = _get_enabled_ops(ops_list, mode)
+    if operators:
+        menu.prepend(draw)
+
+
+def _remove_draw_functions(ops_list, menu):
+    for operator, label in ops_list:
+        draw_function = getattr(operator, 'draw_fun', None)
+        if draw_function:
+            menu.remove(draw_function)
+
+
+def _remove_ops_from_menus(menu_imp, menu_exp):
+    # remove operators from import menu
+    _remove_draw_functions(import_ops, menu_imp)
+
+    # remove operators from export menu
+    _remove_draw_functions(export_ops, menu_exp)
+
+    # remove compact menus
+    menu_imp.remove(_draw_xray_menu_import)
+    menu_exp.remove(_draw_xray_menu_export)
 
 
 def append_menu_func():
     preferences = utils.version.get_preferences()
-    import_menu, export_menu = utils.version.get_import_export_menus()
+    menu_imp, menu_exp = utils.version.get_import_export_menus()
 
-    # remove import menus
-    remove_draw_functions(import_ops, import_menu)
-    # remove export menus
-    remove_draw_functions(export_ops, export_menu)
-    # remove compact menus
-    import_menu.remove(menu_func_xray_import)
-    export_menu.remove(menu_func_xray_export)
+    _remove_ops_from_menus(menu_imp, menu_exp)
 
+    # create compact menus
     if preferences.compact_menus:
-        # create compact import menus
-        enabled_imp_ops = get_enabled_operators(import_ops, 'import')
-        if enabled_imp_ops:
-            import_menu.prepend(menu_func_xray_import)
-        # create compact export menus
-        enabled_exp_ops = get_enabled_operators(export_ops, 'export')
-        if enabled_exp_ops:
-            export_menu.prepend(menu_func_xray_export)
+        _append_compact_draw_functions(
+            import_ops,
+            menu_imp,
+            'import',
+            _draw_xray_menu_import
+        )
+        _append_compact_draw_functions(
+            export_ops,
+            menu_exp,
+            'export',
+            _draw_xray_menu_export
+        )
+
+    # create standart menus
     else:
-        # create standart import menus
-        append_draw_functions(import_ops, import_menu, 'import')
-        # create standart export menus
-        append_draw_functions(export_ops, export_menu, 'export')
+        _append_standart_draw_functions(import_ops, menu_imp, 'import')
+        _append_standart_draw_functions(export_ops, menu_exp, 'export')
 
 
-class XRAY_MT_import(bpy.types.Menu):
+class XRayBaseMenu(bpy.types.Menu):
+    bl_label = ui.base.build_label()
+
+    def draw(self, context):
+        layout = self.layout
+        operators = _get_enabled_ops(self.ops_list, self.mode)
+        for operator in operators:
+            text = utils.draw.build_op_label(operator, compact=True)
+            layout.operator(operator.bl_idname, text=text)
+
+
+class XRAY_MT_import(XRayBaseMenu):
     bl_idname = 'XRAY_MT_import'
-    bl_label = ui.base.build_label()
 
-    def draw(self, context):
-        layout = self.layout
-        import_operators = get_enabled_operators(import_ops, 'import')
-        for id_name, text in import_operators:
-            layout.operator(id_name, text=text)
+    ops_list = import_ops
+    mode = 'import'
 
 
-class XRAY_MT_export(bpy.types.Menu):
+class XRAY_MT_export(XRayBaseMenu):
     bl_idname = 'XRAY_MT_export'
-    bl_label = ui.base.build_label()
 
-    def draw(self, context):
-        layout = self.layout
-        export_operators = get_enabled_operators(export_ops, 'export')
-        for id_name, text in export_operators:
-            layout.operator(id_name, text=text)
+    ops_list = export_ops
+    mode = 'export'
 
 
 classes = (
@@ -138,19 +156,14 @@ classes = (
 def register():
     for clas in classes:
         bpy.utils.register_class(clas)
+
     append_menu_func()
 
 
 def unregister():
-    import_menu, export_menu = utils.version.get_import_export_menus()
+    menu_imp, menu_exp = utils.version.get_import_export_menus()
 
-    # remove import menus
-    remove_draw_functions(import_ops, import_menu)
-    # remove export menus
-    remove_draw_functions(export_ops, export_menu)
-    # remove compact menus
-    import_menu.remove(menu_func_xray_import)
-    export_menu.remove(menu_func_xray_export)
+    _remove_ops_from_menus(menu_imp, menu_exp)
 
     for clas in reversed(classes):
         bpy.utils.unregister_class(clas)
