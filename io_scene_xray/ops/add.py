2af8ef3a5a3241a1835210ccd3872709110066bd
Pavel_Blend <stalkermodkytia@yandex.ru>
2023-01-08 21:27:59 +0300

    resolved #584: create empty for xray-camera

--- io_scene_xray/ops/add.py
+++ io_scene_xray/ops/add.py
@@ -9,6 +9,7 @@ from .. import utils
 
 
 SOC_HUD_FOV = 30.5
+DRAW_SIZE = 0.5
 
 
 class XRAY_OT_add_camera(bpy.types.Operator):
@@ -20,28 +21,53 @@ class XRAY_OT_add_camera(bpy.types.Operator):
         # set object mode
         if context.object:
             bpy.ops.object.mode_set(mode='OBJECT')
+
         # create camera
         camera = bpy.data.cameras.new('xray-camera')
         camera_obj = bpy.data.objects.new('xray-camera', camera)
+
         # link and select
         bpy.ops.object.select_all(action='DESELECT')
         utils.version.link_object(camera_obj)
         utils.version.select_object(camera_obj)
         utils.version.set_active_object(camera_obj)
+
         # set camera object transforms
         camera_obj.location = (0, 0, 0)
-        camera_obj.rotation_mode = 'XYZ'
+        camera_obj.rotation_mode = 'YXZ'
         camera_obj.rotation_euler = (math.pi / 2, 0, 0)
         camera_obj.scale = (1, 1, 1)
-        # set camera settings
+
+        # set camera display settings
         if utils.version.IS_28:
-            camera.display_size = 0.1
+            camera.display_size = DRAW_SIZE
         else:
-            camera.draw_size = 0.1
+            camera.draw_size = DRAW_SIZE
+
+        # set camera settings
         camera.clip_start = 0.2
         camera.lens_unit = 'FOV'
         camera.sensor_fit = 'VERTICAL'
         camera.angle = math.radians(SOC_HUD_FOV)
+
+        # create camera parent
+        parent = bpy.data.objects.new('xray-camera-root', None)
+        parent.rotation_mode = 'YXZ'
+        utils.version.link_object(parent)
+        utils.version.select_object(parent)
+
+        # set empty display settings
+        if utils.version.IS_28:
+            parent.empty_display_size = DRAW_SIZE
+        else:
+            parent.empty_draw_size = DRAW_SIZE
+
+        # set camera parent
+        camera_obj.parent = parent
+
+        # set active camera
+        context.scene.camera = camera_obj
+
         return {'FINISHED'}
 
 
