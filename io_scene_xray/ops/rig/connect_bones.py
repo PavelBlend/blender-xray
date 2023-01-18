a25af06b9a01d8c6147f5884d661da0863b3f0c8
Pavel_Blend <stalkermodkytia@yandex.ru>
2023-01-18 20:29:28 +0300

    refactor 'create connected bones' operator and fixed #598

--- io_scene_xray/ops/rig/connect_bones.py
+++ io_scene_xray/ops/rig/connect_bones.py
@@ -14,76 +14,99 @@ NAME_SUFFIX = ' connected'
 BONE_NAME_SUFFIX = ' c'
 
 
+def set_con_tail_without_verts(bone, connected_bone):
+    # set connected bone tail without vertices
+    parent = bone.parent
+    if parent:
+        offset = (parent.head - bone.head).length / 2
+        direct = (bone.head - parent.head).normalized()
+        tail_offset = direct * offset
+    else:
+        offset = 0.05
+        tail_offset = mathutils.Vector((0, 0, offset))
+    connected_bone.tail = connected_bone.head + tail_offset
+
+
 def connect_bones(arm, mesh_objs):
-    bone_new_parent = {}
+    # collect vertex group
     vertex_groups = {}
-    if mesh_objs:
-        for bone in arm.edit_bones:
-            if not len(bone.children):
-                for mesh_obj in mesh_objs:
-                    for vertex in mesh_obj.data.vertices:
-                        for group in vertex.groups:
-                            vertex_group = mesh_obj.vertex_groups[group.group]
-                            vertex_groups.setdefault(vertex_group.name, []).append((
-                                vertex.co,
-                                group.weight
-                            ))
+    for mesh_obj in mesh_objs:
+        for vertex in mesh_obj.data.vertices:
+            for group in vertex.groups:
+                vert_group = mesh_obj.vertex_groups[group.group]
+                vertex_groups.setdefault(vert_group.name, []).append((
+                    vertex.co,
+                    group.weight
+                ))
+
+    # collect edit bones
     edit_bones = []
     for bone in arm.edit_bones:
         edit_bones.append(bone)
+
+    # create connected bones
+    connected_bones = {}
     for bone in edit_bones:
         children_count = len(bone.children)
         connected_bone = arm.edit_bones.new(name=bone.name + BONE_NAME_SUFFIX)
+
+        # set head coordinate
         connected_bone.head = bone.head
-        if children_count == 1:
+
+        # set tail coordinate
+        if children_count == 1:    # one child
             child_bone = bone.children[0]
             connected_bone.tail = child_bone.head
-        elif children_count > 1:
-            children_heads = []
-            for child_bone in bone.children:
-                children_heads.append(child_bone.head)
+
+        elif children_count > 1:    # many children
             children_sum = mathutils.Vector((0.0, 0.0, 0.0))
-            for child_head in children_heads:
-                children_sum += child_head
-            children_center = children_sum / len(children_heads)
-            connected_bone.tail = (children_center + bone.head) / 2
-        elif not children_count:
+            for child_bone in bone.children:
+                children_sum += child_bone.head
+            children_center = children_sum / len(bone.children)
+            connected_bone.tail = (connected_bone.head + children_center) / 2
+
+        else:    # without children
             if mesh_objs:
                 vertices = vertex_groups.get(bone.name)
-                if not vertices:
-                    continue
-                vertex_sum_offset = mathutils.Vector((0.0, 0.0, 0.0))
-                weights = []
-                for vert_co, weight in vertices:
-                    vertex_sum_offset += (vert_co - connected_bone.head) * weight
-                    weights.append(weight)
-                tail_offset = vertex_sum_offset / sum(weights)
-                connected_bone.tail = connected_bone.head + tail_offset
-            else:
-                parent = bone.parent
-                if parent:
-                    offset = (parent.head - bone.head).length / 2
-                    direct = (bone.head - parent.head).normalized()
-                    tail_offset = direct * offset
+                if vertices:
+                    verts_sum = mathutils.Vector((0.0, 0.0, 0.0))
+                    for vert_co, weight in vertices:
+                        verts_sum += (vert_co - connected_bone.head) * weight
+                    tail_offset = verts_sum / len(vertices)
+                    connected_bone.tail = connected_bone.head + tail_offset
                 else:
-                    offset = 0.05
-                    tail_offset = mathutils.Vector((0, 0, offset))
-                connected_bone.tail = connected_bone.head + tail_offset
-        bone_new_parent[bone] = connected_bone
-    bone_layers = [False, ] * 32
-    old_bone_layers = bone_layers.copy()
-    old_bone_layers[31] = True
-    connected_bone_layers = bone_layers.copy()
-    connected_bone_layers[0] = True
-    for child_bone, parent_bone in bone_new_parent.items():
-        old_parent = child_bone.parent
-        child_bone.parent = parent_bone
-        child_bone.layers = old_bone_layers
-        new_parent = bone_new_parent.get(old_parent, None)
-        if new_parent:
-            parent_bone.parent = new_parent
+                    set_con_tail_without_verts(bone, connected_bone)
+
+            else:
+                set_con_tail_without_verts(bone, connected_bone)
+
+        connected_bones[bone] = connected_bone
+
+    # bone layers
+    arm_layers = [False, ] * 32
+
+    bone_layers = arm_layers.copy()
+    bone_layers[31] = True
+
+    connected_layers = arm_layers.copy()
+    connected_layers[0] = True
+
+    # change bones parents
+    for bone, connected_bone in connected_bones.items():
+
+        # set connected bone parent
+        connected_parent = connected_bones.get(bone.parent, None)
+        if connected_parent:
+            connected_bone.parent = connected_parent
         else:
-            parent_bone.parent = old_parent
+            connected_bone.parent = bone.parent
+
+        # set bone parent
+        bone.parent = connected_bone
+
+        # set layers
+        bone.layers = bone_layers
+        connected_bone.layers = connected_layers
 
 
 class XRAY_OT_create_connected_bones(bpy.types.Operator):
@@ -104,21 +127,28 @@ class XRAY_OT_create_connected_bones(bpy.types.Operator):
     def execute(self, context):
         if context.object:
             bpy.ops.object.mode_set(mode='OBJECT')
+
+        # check input
         src_name = self.source_armature
         if not src_name:
             self.report({'WARNING'}, text.warn.connect_not_spec_arm)
             return {'FINISHED'}
+
         src_arm_obj = bpy.data.objects.get(src_name)
         if not src_arm_obj:
             self.report({'WARNING'}, text.warn.connect_not_found_arm)
             return {'FINISHED'}
+
         if src_arm_obj.type != 'ARMATURE':
             self.report({'WARNING'}, text.warn.connect_is_not_arm)
             return {'FINISHED'}
+
         src_arm = src_arm_obj.data
         if not len(src_arm.bones):
             self.report({'WARNING'}, text.warn.connect_nas_no_bones)
             return {'FINISHED'}
+
+        # create armature
         arm_obj = src_arm_obj.copy()
         arm = src_arm.copy()
         arm_obj.data = arm
@@ -128,10 +158,13 @@ class XRAY_OT_create_connected_bones(bpy.types.Operator):
         bpy.ops.object.select_all(action='DESELECT')
         utils.version.select_object(arm_obj)
         utils.version.set_active_object(arm_obj)
+
         # clear pose bone transforms
         bpy.ops.object.mode_set(mode='POSE')
         for bone in arm_obj.pose.bones:
             bone.matrix_basis = mathutils.Matrix.Identity(4)
+
+        # collect meshes
         arm_user_map = bpy.data.user_map(
             subset={src_arm_obj, },
             value_types={'OBJECT', }
@@ -142,13 +175,17 @@ class XRAY_OT_create_connected_bones(bpy.types.Operator):
             for obj in object_users:
                 if obj.type == 'MESH':
                     mesh_objects.append(obj)
+
+        # connect bones
         bpy.ops.object.mode_set(mode='EDIT')
         connect_bones(arm, mesh_objects)
         bpy.ops.object.mode_set(mode='OBJECT')
+
         # link bones
         utils.version.set_active_object(src_arm_obj)
         bpy.ops.io_scene_xray.link_bones(armature=arm_obj.name)
         utils.version.set_active_object(arm_obj)
+
         return {'FINISHED'}
 
     def invoke(self, context, event):
