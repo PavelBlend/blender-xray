e88c4f33a367fc8c04d9a5b27e82b5e626a24b6e
Pavel_Blend <stalkermodkytia@yandex.ru>
2023-02-13 10:39:19 +0300

    refactor action panel

--- io_scene_xray/panels/action.py
+++ io_scene_xray/panels/action.py
@@ -17,48 +17,57 @@ class XRAY_PT_action(ui.base.XRayPanel):
 
     @classmethod
     def poll(cls, context):
-        preferences = utils.version.get_preferences()
+        obj = context.active_object
+
+        if not obj:
+            return
+
+        if not (obj.animation_data and obj.animation_data.action):
+            return
+
+        pref = utils.version.get_preferences()
+
         panel_used = (
-            # import plugins
-            preferences.enable_object_import or
-            preferences.enable_anm_import or
-            preferences.enable_skls_import or
-            preferences.enable_omf_import or
-            # export plugins
-            preferences.enable_object_export or
-            preferences.enable_anm_export or
-            preferences.enable_skls_export or
-            preferences.enable_skl_export or
-            preferences.enable_omf_export
-        )
-        return (
-            context.active_object and
-            context.active_object.animation_data and
-            context.active_object.animation_data.action and
-            panel_used
+            # import formats
+            pref.enable_object_import or
+            pref.enable_skls_import or
+            pref.enable_ogf_import or
+            pref.enable_omf_import or
+            pref.enable_anm_import or
+
+            # export formats
+            pref.enable_object_export or
+            pref.enable_skls_export or
+            pref.enable_skl_export or
+            pref.enable_ogf_export or
+            pref.enable_omf_export or
+            pref.enable_anm_export
         )
 
-    def draw_anm_bake_props(self, layout, data):
+        return panel_used
+
+    def draw_refine_props(self, layout, data):
+        layout.prop(data, 'autobake_custom_refine', toggle=True)
+
         col = layout.column(align=True)
-        col.label(text='Bake:')
-        row = col.row(align=True)
-        row.prop(data, 'autobake', expand=True)
-        col = col.column(align=True)
-        col.active = data.autobake != 'off'
-        col.prop(data, 'autobake_custom_refine', toggle=True)
-        col = col.column(align=True)
         col.active = data.autobake_custom_refine
+
         col.prop(data, 'autobake_refine_location', text='Location Threshold')
         col.prop(data, 'autobake_refine_rotation', text='Rotation Threshold')
 
-    def draw_skls_bake_props(self, layout, data):
+    def draw_bake_props(self, layout, data, autobake_props):
         col = layout.column(align=True)
+
         col.label(text='Bake:')
-        col.prop(data, 'autobake_custom_refine', toggle=True)
-        col = col.column(align=True)
-        col.active = data.autobake_custom_refine
-        col.prop(data, 'autobake_refine_location', text='Location Threshold')
-        col.prop(data, 'autobake_refine_rotation', text='Rotation Threshold')
+
+        if autobake_props:
+            row = col.row(align=True)
+            row.prop(data, 'autobake', expand=True)
+
+            col = col.column(align=True)
+            col.active = data.autobake != 'off'
+
+        self.draw_refine_props(col, data)
 
     def draw(self, context):
         layout = self.layout
@@ -67,45 +76,78 @@ class XRAY_PT_action(ui.base.XRayPanel):
         data = action.xray
 
         col = layout.column(align=True)
+
         col.prop(data, 'fps', text='FPS')
+
         if obj.type != 'ARMATURE':
-            self.draw_anm_bake_props(layout, data)
+            # *.anm format properties
+            self.draw_bake_props(layout, data, True)
             return
+
         col.prop(data, 'speed', text='Speed')
         col.prop(data, 'accrue', text='Accrue')
         col.prop(data, 'falloff', text='Falloff')
 
         layout.prop(data, 'flags_fx', text='Type FX', toggle=True)
+
+        row = layout.row(align=True)
+
         if data.flags_fx:
-            row = layout.row(align=True)
             row.label(text='Start Bone:')
-            row.prop_search(data, 'bonestart_name', obj.pose, 'bones', text='')
+            row.prop_search(
+                data,
+                'bonestart_name',
+                obj.pose,
+                'bones',
+                text=''
+            )
             layout.prop(data, 'power', text='Power')
+
         else:
-            row = layout.row(align=True)
             row.label(text='Bone Part:')
-            row.prop_search(data, 'bonepart_name', obj.pose, 'bone_groups', text='')
+            row.prop_search(
+                data,
+                'bonepart_name',
+                obj.pose,
+                'bone_groups',
+                text=''
+            )
+
             col = layout.column(align=True)
             row = col.row(align=True)
+
             row.prop(data, 'flags_stopatend', text='Stop', toggle=True)
             row.prop(data, 'flags_nomix', text='No Mix', toggle=True)
             row.prop(data, 'flags_syncpart', text='Sync', toggle=True)
+
             row = col.row(align=True)
             row.prop(data, 'flags_footsteps', text='Foot Steps', toggle=True)
             row.prop(data, 'flags_movexform', text='Move XForm', toggle=True)
+
             row = col.row(align=True)
             row.prop(data, 'flags_idle', text='Idle', toggle=True)
             row.prop(data, 'flags_weaponbone', text='Weapon Bone', toggle=True)
 
+        # *.skls bake settings
         layout.separator()
-        self.draw_skls_bake_props(layout, data)
+        self.draw_bake_props(layout, data, False)
         layout.separator()
+
+        # copy/paste operators
         row = layout.row(align=True)
         row.label(text='Settings:')
         row.operator(ops.action.XRAY_OT_copy_action_settings.bl_idname)
         row.operator(ops.action.XRAY_OT_paste_action_settings.bl_idname)
-        layout.context_pointer_set(formats.skl.ops.XRAY_OT_export_skl.bl_idname + '.action', action)
-        layout.operator(formats.skl.ops.XRAY_OT_export_skl.bl_idname, icon='EXPORT')
+
+        # export operators
+        layout.context_pointer_set(
+            formats.skl.ops.XRAY_OT_export_skl.bl_idname + '.action',
+            action
+        )
+        layout.operator(
+            formats.skl.ops.XRAY_OT_export_skl.bl_idname,
+            icon='EXPORT'
+        )
 
 
 def register():
