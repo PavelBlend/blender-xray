c3fa8d3a92b618f9028916380e07733a06aac125
Pavel_Blend <stalkermodkytia@yandex.ru>
2023-02-24 13:52:42 +0300

    fixed #534: do not specify sun and light in tree materials

--- io_scene_xray/formats/ogf/imp/material.py
+++ io_scene_xray/formats/ogf/imp/material.py
@@ -3,20 +3,20 @@ from .. import fmt
 from ... import level
 
 
-def get_level_material(lvl, shader_id, texture_id):
-    material_key = (shader_id, texture_id)
+def get_level_material(lvl, visual):
+    material_key = (visual.shader_id, visual.texture_id)
     bpy_material = lvl.materials.get(material_key, None)
 
     if bpy_material:
         return bpy_material
 
     if lvl.shaders and lvl.textures:
-        shader_raw = lvl.shaders[shader_id]
-        texture_raw = lvl.textures[texture_id]
+        shader_raw = lvl.shaders[visual.shader_id]
+        texture_raw = lvl.textures[visual.texture_id]
 
     else:
-        shader_raw = lvl.shaders_or_textures[shader_id]
-        texture_raw = lvl.shaders_or_textures[texture_id]
+        shader_raw = lvl.shaders_or_textures[visual.shader_id]
+        texture_raw = lvl.shaders_or_textures[visual.texture_id]
 
     shader_data = shader_raw + '/' + texture_raw
     bpy_material, bpy_image = level.shaders.import_shader(
@@ -25,7 +25,7 @@ def get_level_material(lvl, shader_id, texture_id):
         shader_data
     )
     lvl.materials[material_key] = bpy_material
-    lvl.images[texture_id] = bpy_image
+    lvl.images[visual.texture_id] = bpy_image
 
     return bpy_material
 
@@ -37,6 +37,14 @@ def assign_level_material(bpy_mesh, visual, lvl):
         ):
         shader_id = visual.shader_id
         bpy_material = lvl.materials[shader_id]
+
+        if visual.format_version == fmt.FORMAT_VERSION_4:
+            trees = (fmt.ModelType_v4.TREE_ST, fmt.ModelType_v4.TREE_PM)
+
+            if visual.model_type in trees:
+                bpy_material.xray.light_vert_color = ''
+                bpy_material.xray.sun_vert_color = ''
+
     else:
         bpy_material = get_level_material(
             lvl,
