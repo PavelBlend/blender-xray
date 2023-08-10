24b3fe132f713d63dc0a20df921b6ecfea6fe4e1
PavelBlend <stalkermodkytia@yandex.ru>
2023-08-10 17:31:01 +0300

    v2.32.0

--- io_scene_xray/__init__.py
+++ io_scene_xray/__init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     'name': 'XRay Engine Tools',
     'author': 'Vakhurin Sergey (igel), Pavel_Blend, Viktoria Danchenko',
-    'version': (2, 31, 4),
+    'version': (2, 32, 0),
     'blender': (2, 80, 0),
     'category': 'Import-Export',
     'location': 'File > Import/Export',
