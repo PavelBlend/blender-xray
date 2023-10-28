a8085cab93d32c5f6de563551004b310641c41a3
PavelBlend <stalkermodkytia@yandex.ru>
2023-10-28 21:19:21 +0300

    refactor game level indices buffers import

--- io_scene_xray/formats/level/imp/ib.py
+++ io_scene_xray/formats/level/imp/ib.py
@@ -4,10 +4,13 @@ from .... import rw
 
 
 def import_indices_buffers(data):
-    packed_reader = rw.read.PackedReader(data)
-    indices_buffers_count = packed_reader.uint32()
-    indices_buffers = []
-    for indices_buffer_index in range(indices_buffers_count):
-        indices_buffer, indices_count = ogf.imp.indices.read_ib(packed_reader)
-        indices_buffers.append(indices_buffer)
-    return indices_buffers
+    reader = rw.read.PackedReader(data)
+
+    buffers = []
+    buffers_count = reader.uint32()
+
+    for index in range(buffers_count):
+        buffer, _ = ogf.imp.indices.read_ib(reader)
+        buffers.append(buffer)
+
+    return buffers
