9285e177790c481b5c0ea4dce9c1d01aa40f9bb3
PavelBlend <stalkermodkytia@yandex.ru>
2023-10-29 12:00:58 +0300

    refactor game level lights import

---
+++ io_scene_xray/formats/level/imp/light.py
@@ -0,0 +1,156 @@
+# blender modules
+import bpy
+import mathutils
+
+# addon modules
+from . import create
+from .. import fmt
+from .... import rw
+from .... import utils
+
+
+LIGHT_OBJECT_NAME = 'light'
+INT_MAX = 2 ** 31 - 1
+
+
+def _read_light(packed_reader, data):
+    light_type = packed_reader.uint32()    # type of light source
+    if light_type > INT_MAX:
+        light_type = -1
+    data.light_type = light_type
+
+    data.diffuse = packed_reader.getf('<4f')
+    data.specular = packed_reader.getf('<4f')
+    data.ambient = packed_reader.getf('<4f')
+
+    position = packed_reader.getv3f()
+    direction = packed_reader.getv3f()
+
+    data.range_ = packed_reader.getf('<f')[0]    # cutoff range
+    data.falloff = packed_reader.getf('<f')[0]
+    data.attenuation_0 = packed_reader.getf('<f')[0]    # constant attenuation
+    data.attenuation_1 = packed_reader.getf('<f')[0]    # linear attenuation
+    data.attenuation_2 = packed_reader.getf('<f')[0]    # quadratic attenuation
+    data.theta = packed_reader.getf('<f')[0]    # inner angle of spotlight cone
+    data.phi = packed_reader.getf('<f')[0]    # outer angle of spotlight cone
+
+    return position, direction
+
+
+def _set_light_transforms(light_object, position, direction):
+    dir_vec = mathutils.Vector(direction)
+    euler = dir_vec.to_track_quat('Y', 'Z').to_euler('XYZ')
+    light_object.location = position
+    light_object.rotation_euler = euler
+
+
+def _import_light_v9(packed_reader, light_object):
+    data = light_object.xray.level
+
+    # controller id
+    controller_id = packed_reader.uint32()    # ???
+    if controller_id > INT_MAX:
+        controller_id = -1
+    data.controller_id = controller_id
+
+    position, direction = _read_light(packed_reader, data)
+
+    _set_light_transforms(light_object, position, direction)
+
+
+def _import_light_v8(packed_reader, light_object):
+    data = light_object.xray.level
+    position, direction = _read_light(packed_reader, data)
+
+    dw_frame, flags = packed_reader.getf('<2I')
+    affect_static = bool(flags & fmt.FLAG_AFFECT_STATIC)
+    affect_dynamic = bool(flags & fmt.FLAG_AFFECT_DYNAMIC)
+    procedural = bool(flags & fmt.FLAG_PROCEDURAL)
+
+    name = packed_reader.getf('<{}s'.format(fmt.LIGHT_V8_NAME_LEN))
+
+    if data.light_type == fmt.D3D_LIGHT_POINT:
+        data.controller_id = 2
+    elif data.light_type == fmt.D3D_LIGHT_DIRECTIONAL:
+        data.controller_id = 1
+
+    _set_light_transforms(light_object, position, direction)
+
+
+def _import_light_v5(packed_reader, light_object):
+    data = light_object.xray.level
+    position, direction = _read_light(packed_reader, data)
+
+    dw_frame, flags = packed_reader.getf('<2I')
+    current_time, speed = packed_reader.getf('<2f')
+    key_start, key_count = packed_reader.getf('<2H')
+
+    if data.light_type == fmt.D3D_LIGHT_POINT:
+        data.controller_id = 2
+    elif data.light_type == fmt.D3D_LIGHT_DIRECTIONAL:
+        data.controller_id = 1
+
+    _set_light_transforms(light_object, position, direction)
+
+
+def _create_light_object(light_index, collection, lights_object):
+    object_name = '{0}_{1:0>3}'.format(LIGHT_OBJECT_NAME, light_index)
+
+    if utils.version.IS_28:
+        bpy_data = bpy.data.lights
+    else:
+        bpy_data = bpy.data.lamps
+
+    light = bpy_data.new(object_name, 'SPOT')
+    light_object = create.create_object(object_name, light)
+
+    light_object.parent = lights_object
+    light_object.xray.is_level = True
+    light_object.xray.level.object_type = 'LIGHT_DYNAMIC'
+
+    collection.objects.link(light_object)
+    if not utils.version.IS_28:
+        utils.version.link_object(light_object)
+
+    return light_object
+
+
+def _create_lights_object(level_object, collection):
+    lights_object = create.create_object('lights', None)
+
+    lights_object.parent = level_object
+    level_object.xray.level.lights_obj = lights_object.name
+
+    collection.objects.link(lights_object)
+    if not utils.version.IS_28:
+        utils.version.link_object(lights_object)
+
+    return lights_object
+
+
+def import_lights(data, level, level_object):
+    packed_reader = rw.read.PackedReader(data)
+    collection = level.collections[create.LEVEL_LIGHTS_COLLECTION_NAME]
+
+    # create lights root-object
+    lights_obj = _create_lights_object(level_object, collection)
+
+    # search import function and light size
+    if level.xrlc_version >= fmt.VERSION_9:
+        light_size = fmt.LIGHT_DYNAMIC_SIZE_V9
+        import_light_funct = _import_light_v9
+
+    elif level.xrlc_version == fmt.VERSION_8:
+        light_size = fmt.LIGHT_DYNAMIC_SIZE_V8
+        import_light_funct = _import_light_v8
+
+    else:
+        light_size = fmt.LIGHT_DYNAMIC_SIZE_V5
+        import_light_funct = _import_light_v5
+
+    # import light objects
+    light_count = len(data) // light_size
+
+    for index in range(light_count):
+        light_obj = _create_light_object(index, collection, lights_obj)
+        import_light_funct(packed_reader, light_obj)
--- io_scene_xray/formats/level/imp/main.py
+++ io_scene_xray/formats/level/imp/main.py
@@ -15,6 +15,7 @@ from . import ib
 from . import swi
 from . import cform
 from . import geom
+from . import light
 from . import glow
 from . import sector
 from . import portal
@@ -49,148 +50,7 @@ class Level(object):
         self.sectors_objects = {}
 
 
-INT_MAX = 2 ** 31 - 1
-
-
-def import_light_dynamic(packed_reader, light_object):
-    data = light_object.xray.level
-    data.object_type = 'LIGHT_DYNAMIC'
-    light_object.xray.is_level = True
-
-    # controller id
-    controller_id = packed_reader.uint32() # ???
-    if controller_id > INT_MAX:
-        controller_id = -1
-    data.controller_id = controller_id
-
-    position, direction = read_light(packed_reader, data)
-
-    dir_vec = mathutils.Vector((direction[0], direction[2], direction[1]))
-    euler = dir_vec.to_track_quat('Y', 'Z').to_euler('XYZ')
-    light_object.location = position[0], position[2], position[1]
-    light_object.rotation_euler = euler[0], euler[1], euler[2]
-
-
-def read_light(packed_reader, data):
-    light_type = packed_reader.uint32()    # type of light source
-    if light_type > INT_MAX:
-        light_type = -1
-    data.light_type = light_type
-
-    data.diffuse = packed_reader.getf('<4f')
-    data.specular = packed_reader.getf('<4f')
-    data.ambient = packed_reader.getf('<4f')
-
-    position = packed_reader.getf('<3f')
-    direction = packed_reader.getf('<3f')
-
-    data.range_ = packed_reader.getf('<f')[0]    # cutoff range
-    data.falloff = packed_reader.getf('<f')[0]
-    data.attenuation_0 = packed_reader.getf('<f')[0]    # constant attenuation
-    data.attenuation_1 = packed_reader.getf('<f')[0]    # linear attenuation
-    data.attenuation_2 = packed_reader.getf('<f')[0]    # quadratic attenuation
-    data.theta = packed_reader.getf('<f')[0]    # inner angle of spotlight cone
-    data.phi = packed_reader.getf('<f')[0]    # outer angle of spotlight cone
-
-    return position, direction
-
-
-def import_light_dynamic_v8(packed_reader, light_object):
-    light_object.xray.is_level = True
-    data = light_object.xray.level
-    data.object_type = 'LIGHT_DYNAMIC'
-
-    position, direction = read_light(packed_reader, data)
-
-    dw_frame = packed_reader.getf('<I')[0]
-    flags = packed_reader.getf('<I')[0]
-    affect_static = bool(flags & fmt.FLAG_AFFECT_STATIC)
-    affect_dynamic = bool(flags & fmt.FLAG_AFFECT_DYNAMIC)
-    procedural = bool(flags & fmt.FLAG_PROCEDURAL)
-
-    name = packed_reader.getf('<{}s'.format(fmt.LIGHT_V8_NAME_LEN))
-
-    if data.light_type == fmt.D3D_LIGHT_POINT:
-        data.controller_id = 2
-    elif data.light_type == fmt.D3D_LIGHT_DIRECTIONAL:
-        data.controller_id = 1
-
-    dir_vec = mathutils.Vector((direction[0], direction[2], direction[1]))
-    euler = dir_vec.to_track_quat('Y', 'Z').to_euler('XYZ')
-    light_object.location = position[0], position[2], position[1]
-    light_object.rotation_euler = euler[0], euler[1], euler[2]
-
-
-def import_light_dynamic_v5(packed_reader, light_object):
-    light_object.xray.is_level = True
-    data = light_object.xray.level
-    data.object_type = 'LIGHT_DYNAMIC'
-
-    position, direction = read_light(packed_reader, data)
-
-    dw_frame, flags = packed_reader.getf('<2I')
-    current_time, speed = packed_reader.getf('<2f')
-    key_start, key_count = packed_reader.getf('<2H')
-
-    if data.light_type == fmt.D3D_LIGHT_POINT:
-        data.controller_id = 2
-    elif data.light_type == fmt.D3D_LIGHT_DIRECTIONAL:
-        data.controller_id = 1
-
-    dir_vec = mathutils.Vector((direction[0], direction[2], direction[1]))
-    euler = dir_vec.to_track_quat('Y', 'Z').to_euler('XYZ')
-    light_object.location = position[0], position[2], position[1]
-    light_object.rotation_euler = euler[0], euler[1], euler[2]
-
-
-def create_light_object(light_index, collection):
-    object_name = 'light_dynamic_{:0>3}'.format(light_index)
-    if utils.version.IS_28:
-        bpy_data = bpy.data.lights
-    else:
-        bpy_data = bpy.data.lamps
-    light = bpy_data.new(object_name, 'SPOT')
-    bpy_object = create.create_object(object_name, light)
-    collection.objects.link(bpy_object)
-    if not utils.version.IS_28:
-        utils.version.link_object(bpy_object)
-    return bpy_object
-
-
-def create_lights_object(collection):
-    object_name = 'light dynamic'
-    bpy_object = create.create_object(object_name, None)
-    collection.objects.link(bpy_object)
-    if not utils.version.IS_28:
-        utils.version.link_object(bpy_object)
-    return bpy_object
-
-
-def import_lights(data, level, level_object):
-    packed_reader = rw.read.PackedReader(data)
-    collection = level.collections[create.LEVEL_LIGHTS_COLLECTION_NAME]
-    lights_object = create_lights_object(collection)
-    lights_object.parent = level_object
-    level_object.xray.level.lights_obj = lights_object.name
-
-    if level.xrlc_version > fmt.VERSION_8:
-        light_size = fmt.LIGHT_DYNAMIC_SIZE
-        import_light_funct = import_light_dynamic
-    elif level.xrlc_version == fmt.VERSION_8:
-        light_size = fmt.LIGHT_DYNAMIC_SIZE_V8
-        import_light_funct = import_light_dynamic_v8
-    else:
-        light_size = fmt.LIGHT_DYNAMIC_SIZE_V5
-        import_light_funct = import_light_dynamic_v5
-
-    light_count = len(data) // light_size
-    for light_index in range(light_count):
-        light_object = create_light_object(light_index, collection)
-        import_light_funct(packed_reader, light_object)
-        light_object.parent = lights_object
-
-
-def import_level(level, context, chunks):
+def _import_level(level, context, chunks):
     # find chunks ids
     if level.xrlc_version >= fmt.VERSION_13:
         chunks_ids = fmt.Chunks13
@@ -275,7 +135,7 @@ def import_level(level, context, chunks):
 
     # lights
     lights_data = chunks.pop(chunks_ids.LIGHT_DYNAMIC)
-    import_lights(lights_data, level, level_object)
+    light.import_lights(lights_data, level, level_object)
 
     # cform
     if level.xrlc_version >= fmt.VERSION_10:
@@ -295,7 +155,7 @@ def import_level(level, context, chunks):
         ))
 
 
-def import_main(context, level):
+def _import_main(context, level):
     # level chunks
     level_reader = rw.utils.get_file_reader(level.file, chunked=True)
     chunks = rw.utils.get_reader_chunks(level_reader)
@@ -308,7 +168,7 @@ def import_main(context, level):
     chunks.update(geom_chunks)
 
     # import level
-    import_level(level, context, chunks)
+    _import_level(level, context, chunks)
 
 
 @log.with_context(name='import-game-level')
@@ -325,4 +185,4 @@ def import_file(context):
 
     context.level_name = level.name
 
-    import_main(context, level)
+    _import_main(context, level)
