5adfba87e03af8af16a1e98accb51e228bbc8a56
PavelBlend <stalkermodkytia@yandex.ru>
2023-10-30 17:31:31 +0300

    refactor game level vertex buffers import

--- io_scene_xray/formats/level/imp/vb.py
+++ io_scene_xray/formats/level/imp/vb.py
@@ -21,14 +21,136 @@ class VertexBuffer:
         self.float_normals = False
 
 
+class Code:
+    def __init__(self):
+        self.tabs = 0
+        self.lines = []
+
+    def add_indent(self):
+        ''' Add 4 spaces indentation level. '''
+        self.tabs += 1
+
+    def remove_indent(self):
+        ''' Remove 4 spaces indentation level. '''
+        self.tabs -= 1
+        if self.tabs < 0:
+            self.tabs = 0
+
+    def add_line(self, line):
+        ''' Add a line of code to the end. '''
+        self.lines.append(' ' * self.tabs * 4 + line)
+
+    def get_code(self):
+        ''' Get full code. '''
+        code = self._get_merged_lines()
+        return code
+
+    def _get_merged_lines(self):
+        return '\n'.join(self.lines)
+
+
 UV_CORRECTOR = '({} / 255) * (32 / 0x8000)'
 CORRECTOR_U = UV_CORRECTOR.format('correct_u')
 CORRECTOR_V = UV_CORRECTOR.format('correct_v')
 
 
+def _get_tex_uv_code(code, ver, data_type, reader_code, has_corr):
+    if data_type == fmt.FLOAT2:
+        code.add_line('coord_u, coord_v' + reader_code)
+        code.add_line('vb.uv.append((coord_u, 1 - coord_v))')
+
+    elif data_type == fmt.SHORT2:
+        code.add_line('coord_u, coord_v' + reader_code)
+
+        if has_corr:
+            code.add_line(
+                'vb.uv.append(('
+                    'coord_u / fmt.UV_COEFFICIENT + {0}, '
+                    '1 - coord_v / fmt.UV_COEFFICIENT - {1}'
+                '))'.format(
+                    CORRECTOR_U,
+                    CORRECTOR_V
+                )
+            )
+        else:
+            code.add_line(
+                'vb.uv.append(('
+                    'coord_u / fmt.UV_COEFFICIENT, '
+                    '1 - coord_v / fmt.UV_COEFFICIENT'
+                '))'
+            )
+
+    elif data_type == fmt.SHORT4:
+
+        if ver >= fmt.VERSION_12:
+            # MU meshes
+            code.add_line(
+                'coord_u, coord_v, wind_coef, _' + reader_code
+            )
+
+            if has_corr:
+                code.add_line(
+                    'vb.uv.append(('
+                        'coord_u / fmt.UV_COEFFICIENT_2 + {0}, '
+                        '1 - coord_v / fmt.UV_COEFFICIENT_2 - {1}'
+                    '))'.format(
+                        CORRECTOR_U,
+                        CORRECTOR_V
+                    )
+                )
+            else:
+                code.add_line(
+                    'vb.uv.append(('
+                        'coord_u / fmt.UV_COEFFICIENT_2, '
+                        '1 - coord_v / fmt.UV_COEFFICIENT_2'
+                    '))'
+                )
+
+        else:
+            code.add_line('coord_u, coord_v, lmap_u, lmap_v' + reader_code)
+            code.add_line(
+                'vb.uv.append(('
+                    'coord_u / fmt.UV_COEFFICIENT, '
+                    '1 - coord_v / fmt.UV_COEFFICIENT'
+                '))'
+            )
+            code.add_line(
+                'vb.uv_lmap.append(('
+                    'lmap_u / fmt.LIGHT_MAP_UV_COEFFICIENT, '
+                    '1 - lmap_v / fmt.LIGHT_MAP_UV_COEFFICIENT'
+                '))'
+            )
+
+
+def _get_uv_code(code, ver, data_type, usage_index, reader_code, has_corr):
+    # texture uv
+    if usage_index == 0:
+        _get_tex_uv_code(code, ver, data_type, reader_code, has_corr)
+
+    # lmap uv
+    elif usage_index == 1:
+        code.add_line('lmap_u, lmap_v' + reader_code)
+
+        if data_type == fmt.SHORT2:
+            code.add_line('lmap_u /= fmt.LIGHT_MAP_UV_COEFFICIENT')
+            code.add_line('lmap_v /= fmt.LIGHT_MAP_UV_COEFFICIENT')
+
+        code.add_line('vb.uv_lmap.append((lmap_u, 1 - lmap_v))')
+
+    else:
+        raise ValueError(
+            'Unsupported UV usage index: {}'.format(usage_index)
+        )
+
+
 def _import_vertices_d3d9(ver, reader, vb, verts_count, usage_list):
-    code = 'for vertex_index in range({}):\n'.format(verts_count)
-    has_uv_corrector = False
+    code = Code()
+
+    code.add_line('for _ in range({}):'.format(verts_count))
+    code.add_indent()
+
+    # has uv corrector
+    has_corr = False
 
     for usage_info in usage_list:
 
@@ -39,92 +161,54 @@ def _import_vertices_d3d9(ver, reader, vb, verts_count, usage_list):
         data_type = fmt.types[data_type]
         usage = fmt.usage[usage]
 
+        reader_code = ' = reader.getf("<{}")'.format(data_format)
+
         # position
         if usage == fmt.POSITION:
-            code += '    coord_x, coord_y, coord_z = reader.getf("<{}")\n'.format(data_format)
-            code += '    vb.position.append((coord_x, coord_z, coord_y))\n'
+            code.add_line('pos_x, pos_y, pos_z' + reader_code)
+            code.add_line('vb.position.append((pos_x, pos_z, pos_y))')
 
         # normal
         elif usage == fmt.NORMAL:
-            code += '    norm_x, norm_y, norm_z, hemi = reader.getf("<{}")\n'.format(data_format)
-            code += '    vb.normal.append((norm_z, norm_x, norm_y))\n'
-            code += '    vb.color_hemi.append(hemi / 255)\n'
+            code.add_line('norm_x, norm_y, norm_z, hemi' + reader_code)
+            code.add_line('vb.normal.append((norm_z, norm_x, norm_y))')
+            code.add_line('vb.color_hemi.append(hemi / 255)')
 
         # tangent
         elif usage == fmt.TANGENT:
-            has_uv_corrector = True
-            code += '    tan_x, tan_y, tan_z, correct_u = reader.getf("<{}")\n'.format(data_format)
+            has_corr = True
+            code.add_line('tan_x, tan_y, tan_z, correct_u' + reader_code)
 
         # binormal
         elif usage == fmt.BINORMAL:
-            has_uv_corrector = True
-            code += '    bin_x, bin_y, bin_z, correct_v = reader.getf("<{}")\n'.format(data_format)
+            has_corr = True
+            code.add_line('bin_x, bin_y, bin_z, correct_v' + reader_code)
 
+        # uv
         elif usage == fmt.TEXCOORD:
-
-            # texture uv
-            if usage_index == 0:
-
-                if data_type == fmt.FLOAT2:
-                    code += '    coord_u, coord_v = reader.getf("<{}")\n'.format(data_format)
-                    code += '    vb.uv.append((coord_u, 1 - coord_v))\n'
-
-                elif data_type == fmt.SHORT2:
-                    code += '    coord_u, coord_v = reader.getf("<{}")\n'.format(data_format)
-
-                    if has_uv_corrector:
-                        code += '    vb.uv.append((coord_u / fmt.UV_COEFFICIENT + {0}, 1 - coord_v / fmt.UV_COEFFICIENT - {1}))\n'.format(
-                            CORRECTOR_U,
-                            CORRECTOR_V
-                        )
-                    else:
-                        code += '    vb.uv.append((coord_u / fmt.UV_COEFFICIENT, 1 - coord_v / fmt.UV_COEFFICIENT))\n'
-
-                elif data_type == fmt.SHORT4:
-
-                    if ver >= fmt.VERSION_12:
-                        # MU meshes
-                        code += '    coord_u, coord_v, shader_data, unused = reader.getf("<{}")\n'.format(data_format)
-
-                        if has_uv_corrector:
-                            code += '    vb.uv.append((coord_u / fmt.UV_COEFFICIENT_2 + {0}, 1 - coord_v / fmt.UV_COEFFICIENT_2 - {1}))\n'.format(
-                                CORRECTOR_U,
-                                CORRECTOR_V
-                            )
-                        else:
-                            code += '    vb.uv.append((coord_u / fmt.UV_COEFFICIENT_2, 1 - coord_v / fmt.UV_COEFFICIENT_2))\n'
-
-                    else:
-                        code += '    coord_u, coord_v, lmap_u, lmap_v = reader.getf("<{}")\n'.format(data_format)
-                        code += '    vb.uv.append((coord_u / fmt.UV_COEFFICIENT, 1 - coord_v / fmt.UV_COEFFICIENT))\n'
-                        code += '    vb.uv_lmap.append((lmap_u / fmt.LIGHT_MAP_UV_COEFFICIENT, 1 - lmap_v / fmt.LIGHT_MAP_UV_COEFFICIENT))\n'
-
-            # lmap uv
-            elif usage_index == 1:
-                code += '    lmap_u, lmap_v = reader.getf("<{}")\n'.format(data_format)
-
-                if data_type == fmt.SHORT2:
-                    code += '    lmap_u /= fmt.LIGHT_MAP_UV_COEFFICIENT\n'
-                    code += '    lmap_v /= fmt.LIGHT_MAP_UV_COEFFICIENT\n'
-
-                code += '    vb.uv_lmap.append((lmap_u, 1 - lmap_v))\n'
-
-            else:
-                raise ValueError('Unsupported uv usage index: {}'.format(usage_index))
+            _get_uv_code(
+                code,
+                ver,
+                data_type,
+                usage_index,
+                reader_code,
+                has_corr
+            )
 
         # vertex color
         elif usage == fmt.COLOR:
-            code += '    blue, green, red, sun = reader.getf("<{}")\n'.format(data_format)
+            code.add_line('blue, green, red, sun' + reader_code)
 
             if data_type == fmt.D3DCOLOR:
-                code += '    red /= 255\n'
-                code += '    green /= 255\n'
-                code += '    blue /= 255\n'
+                code.add_line('red /= 255')
+                code.add_line('green /= 255')
+                code.add_line('blue /= 255')
+                code.add_line('sun /= 255')
 
-            code += '    vb.color_light.append((red, green, blue))\n'
-            code += '    vb.color_sun.append(sun)\n'
+            code.add_line('vb.color_light.append((red, green, blue))')
+            code.add_line('vb.color_sun.append(sun)')
 
-    exec(code)
+    exec(code.get_code())
 
 
 def _import_vertices_d3d7(ver, reader, vb, verts_count, vert_fmt):
