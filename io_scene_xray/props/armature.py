9d48f19af76ed17662372ce0b8ccb3e9d59f65a0
PavelBlend <stalkermodkytia@yandex.ru>
2023-11-01 12:11:32 +0300

    refactor armature properties

--- io_scene_xray/props/armature.py
+++ io_scene_xray/props/armature.py
@@ -8,43 +8,63 @@ import bpy
 from .. import utils
 
 
-joint_limit_type_items = (
-    ('IK', 'IK', ''),
-    # Added a space to the beginning and end of
-    # the string so that the translation does not work.
-    ('XRAY', ' X-Ray ', '')
-)
-xray_armature_properties = {
+arm_props = {
     'display_bone_shapes': bpy.props.BoolProperty(
-        name='Display Bone Shapes', default=False
+        name='Display Bone Shapes',
+        default=False
     ),
     'display_bone_mass_centers': bpy.props.BoolProperty(
-        name='Display Bone Mass Centers', default=False
+        name='Display Bone Mass Centers',
+        default=False
     ),
     'bone_mass_center_cross_size': bpy.props.FloatProperty(
-        name='Cross Size', default=0.02, min=0.00001, precision=5
+        name='Cross Size',
+        default=0.02,
+        min=0.00001,
+        precision=5
     ),
     'joint_limits_type': bpy.props.EnumProperty(
-        items=joint_limit_type_items, name='Export Limits From', default='IK'
+        items=(
+            ('IK', 'IK', ''),
+            # Added a space to the beginning and end of
+            # the string so that the translation does not work.
+            ('XRAY', ' X-Ray ', '')
+        ),
+        name='Export Limits From',
+        default='IK'
     ),
     'display_bone_limits': bpy.props.BoolProperty(
-        name='Display Bone Limits', default=False
+        name='Display Bone Limits',
+        default=False
     ),
     'display_bone_limits_radius': bpy.props.FloatProperty(
-        name='Gizmo Radius', default=0.1, min=0.00001, precision=5
+        name='Gizmo Radius',
+        default=0.1,
+        min=0.00001,
+        precision=5
     ),
-    'display_bone_limit_x': bpy.props.BoolProperty(name='Limit X', default=True),
-    'display_bone_limit_y': bpy.props.BoolProperty(name='Limit Y', default=True),
-    'display_bone_limit_z': bpy.props.BoolProperty(name='Limit Z', default=True)
+    'display_bone_limit_x': bpy.props.BoolProperty(
+        name='Limit X',
+        default=True
+    ),
+    'display_bone_limit_y': bpy.props.BoolProperty(
+        name='Limit Y',
+        default=True
+    ),
+    'display_bone_limit_z': bpy.props.BoolProperty(
+        name='Limit Z',
+        default=True
+    )
 }
 
 
-class XRayArmatureProperties(bpy.types.PropertyGroup):
+class XRayArmatureProps(bpy.types.PropertyGroup):
     b_type = bpy.types.Armature
+    props = arm_props
 
     if not utils.version.IS_28:
-        for prop_name, prop_value in xray_armature_properties.items():
-            exec('{0} = xray_armature_properties.get("{0}")'.format(prop_name))
+        for prop_name, prop_value in arm_props.items():
+            exec('{0} = arm_props.get("{0}")'.format(prop_name))
 
     def check_different_version_bones(self):
         return functools.reduce(
@@ -57,21 +77,9 @@ class XRayArmatureProperties(bpy.types.PropertyGroup):
         )
 
 
-prop_groups = (
-    (XRayArmatureProperties, xray_armature_properties),
-)
-
-
 def register():
-    for prop_group, props in prop_groups:
-        utils.version.assign_props([
-            (props, prop_group),
-        ])
-    bpy.utils.register_class(prop_group)
-    prop_group.b_type.xray = bpy.props.PointerProperty(type=prop_group)
+    utils.version.register_prop_group(XRayArmatureProps)
 
 
 def unregister():
-    for prop_group, props in reversed(prop_groups):
-        del prop_group.b_type.xray
-        bpy.utils.unregister_class(prop_group)
+    utils.version.unregister_prop_group(XRayArmatureProps)
