fcbc3b408bdbaf62e34688464cbfac8e7cdb7b3e
PavelBlend <stalkermodkytia@yandex.ru>
2023-12-08 15:16:56 +0300

    refactor part dump

--- utils/dump_part.py
+++ utils/dump_part.py
@@ -1,5 +1,23 @@
+import os
 import le
 
 
-path = 'E:\\sdk\\rawdata\\maps\\l01_v2_ai\\scene_object.part'
-le.part.dump_main(path)
+# search *.part files
+
+part_files = []
+
+for root, dirs, files in os.walk(os.curdir):
+
+    for file in files:
+        ext = os.path.splitext(file)[-1]
+
+        if ext == '.part':
+            path = os.path.join(root, file)
+            path = os.path.abspath(path)
+            part_files.append(path)
+
+# dump files
+
+for path in reversed(part_files):
+    print('dump file: "{}"'.format(path))
+    le.part.dump_main(path)
