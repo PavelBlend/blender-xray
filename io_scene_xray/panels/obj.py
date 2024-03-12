4749a8470ec7b4ea645153c4b92a04b2f9db934c
PavelBlend <stalkermodkytia@yandex.ru>
2024-03-12 10:14:49 +0300

    refactor

--- io_scene_xray/panels/obj.py
+++ io_scene_xray/panels/obj.py
@@ -25,22 +25,29 @@ class XRAY_OT_prop_clip(utils.ie.BaseOperator):
     def execute(self, context):
         *path, prop = self.path.split('.')
         obj = context
+
         for name in path:
             obj = getattr(obj, name)
+
         if self.oper == 'copy':
             context.window_manager.clipboard = getattr(obj, prop)
+
         elif self.oper == 'paste':
             setattr(obj, prop, context.window_manager.clipboard)
+
         elif self.oper == 'clear':
             setattr(obj, prop, '')
+
         return {'FINISHED'}
 
     @classmethod
     def drawall(cls, layout, path, value):
         for item in items:
             row = layout.row(align=True)
+
             if item[0] in ('copy', 'clear') and not value:
                 row.enabled = False
+
             props = row.operator(cls.bl_idname, icon=item[3])
             props.oper = item[0]
             props.path = path
@@ -80,15 +87,265 @@ def draw_motion_list_elements(layout):
 
 
 def draw_motion_refs_elements(layout):
-    layout.operator(ui.motion_refs.XRAY_OT_remove_all_motion_refs.bl_idname, text='', icon='X')
-    layout.operator(ui.motion_refs.XRAY_OT_copy_motion_refs_list.bl_idname, text='', icon='COPYDOWN')
-    layout.operator(ui.motion_refs.XRAY_OT_paste_motion_refs_list.bl_idname, text='', icon='PASTEDOWN')
-    layout.operator(ui.motion_refs.XRAY_OT_sort_motion_refs_list.bl_idname, text='', icon='SORTALPHA')
-    layout.operator(ui.motion_refs.XRAY_OT_add_motion_ref_from_file.bl_idname, text='', icon='FILE_FOLDER')
+    layout.operator(
+        ui.motion_refs.XRAY_OT_remove_all_motion_refs.bl_idname,
+        text='',
+        icon='X'
+    )
+    layout.operator(
+        ui.motion_refs.XRAY_OT_copy_motion_refs_list.bl_idname,
+        text='',
+        icon='COPYDOWN'
+    )
+    layout.operator(
+        ui.motion_refs.XRAY_OT_paste_motion_refs_list.bl_idname,
+        text='',
+        icon='PASTEDOWN'
+    )
+    layout.operator(
+        ui.motion_refs.XRAY_OT_sort_motion_refs_list.bl_idname,
+        text='',
+        icon='SORTALPHA'
+    )
+    layout.operator(
+        ui.motion_refs.XRAY_OT_add_motion_ref_from_file.bl_idname,
+        text='',
+        icon='FILE_FOLDER'
+    )
 
 
-def details_draw_function(self, context):
-    box = self.layout.box()
+def draw_object_props(layout, data):
+    object_box = layout.box()
+
+    # object type
+    split = utils.version.layout_split(object_box, 0.333)
+    split.label(text='Type:')
+    split.prop(data, 'flags_simple', text='')
+
+    if data.flags_use_custom:
+
+        # static/dynamic
+        row = object_box.row(align=True)
+        row.prop(data, 'flags_custom_type', expand=True)
+
+        # flags
+        col = object_box.column(align=True)
+
+        row = col.row(align=True)
+
+        row.prop(
+            data,
+            'flags_custom_progressive',
+            text='Progressive',
+            toggle=True
+        )
+        row.prop(data, 'flags_custom_lod', text='LOD', toggle=True)
+        row.prop(data, 'flags_custom_hom', text='HOM', toggle=True)
+
+        row = col.row(align=True)
+
+        row.prop(
+            data,
+            'flags_custom_musage',
+            text='Multiple Usage',
+            toggle=True
+        )
+        row.prop(
+            data,
+            'flags_custom_soccl',
+            text='Sound Occluder',
+            toggle=True
+        )
+
+    # high quality export
+    object_box.prop(data, 'flags_custom_hqexp', text='HQ Export')
+
+    # lod reference
+    split = utils.version.layout_split(object_box, 0.333)
+    split.label(text='LOD Reference:')
+    split.prop(data, 'lodref', text='')
+
+    # export path
+    split = utils.version.layout_split(object_box, 0.333)
+    split.label(text='Export Path:')
+    row = split.row(align=True)
+
+    row.prop(data, 'export_path', text='')
+
+    row.operator(
+        ops.obj.XRAY_OT_set_export_path.bl_idname,
+        text='',
+        icon='FILE_FOLDER'
+    )
+
+    # user data
+    row, box = ui.collapsible.draw(
+        object_box,
+        'object:userdata',
+        'User Data',
+        enabled=data.userdata != '',
+        icon='VIEWZOOM'
+    )
+    XRAY_OT_prop_clip.drawall(row, 'object.xray.userdata', data.userdata)
+
+    if box:
+
+        if data.userdata:
+            box = box.column(align=True)
+            for line in data.userdata.splitlines():
+                box.label(text=line)
+
+        else:
+            ui.collapsible.XRAY_OT_collaps.set_value('object:userdata', False)
+
+    # motions list
+    if data.motions:
+        split = object_box.split()
+        split.alert = True
+        split.prop(data, 'motions')
+
+    _, box = ui.collapsible.draw(
+        object_box,
+        'object:motions',
+        'Motions ({})'.format(len(data.motions_collection))
+    )
+
+    if box:
+        col = box.column(align=True)
+
+        # play active motion
+        col.prop(data, 'play_active_motion', toggle=True, icon='PLAY')
+
+        # use custom names
+        col.prop(
+            data,
+            'use_custom_motion_names',
+            toggle=True,
+            icon='SORTALPHA'
+        )
+
+        # show motion names
+        names_row = col.row()
+        names_row.label(text='Show:')
+        names_row.prop(data, 'show_motions_names', expand=True)
+
+        # dependency object
+        split = utils.version.layout_split(col, 0.333)
+        split.label(text='Dependency:')
+        split.prop_search(
+            data,
+            'dependency_object',
+            bpy.data,
+            'objects',
+            text=''
+        )
+
+        # motions
+        row = box.row()
+        row.template_list(
+            ui.motion_list.XRAY_UL_motion_list.bl_idname,
+            'name',
+            data,
+            'motions_collection',
+            data,
+            'motions_collection_index',
+            rows=9
+        )
+
+        # motion list operators
+        col = row.column(align=True)
+        ui.list_helper.draw_list_ops(
+            col,
+            data,
+            'motions_collection',
+            'motions_collection_index',
+            custom_elements_func=draw_motion_list_elements
+        )
+
+    # motion references
+    if data.motionrefs:
+        split = object_box.split()
+        split.alert = True
+        split.prop(data, 'motionrefs')
+
+    _, box = ui.collapsible.draw(
+        object_box,
+        'object:motionsrefs',
+        'Motion Refs ({})'.format(len(data.motionrefs_collection))
+    )
+
+    if box:
+
+        # load active motion reference
+        box.prop(data, 'load_active_motion_refs', toggle=True)
+
+        if data.load_active_motion_refs:
+            row = box.row()
+            row.label(text='Format:')
+            row.prop(data.motions_browser, 'file_format', expand=True)
+
+        # motion references
+        row = box.row()
+        row.template_list(
+            'UI_UL_list',
+            'name',
+            data,
+            'motionrefs_collection',
+            data,
+            'motionrefs_collection_index',
+            rows=8
+        )
+
+        # motion reference operators
+        col = row.column(align=True)
+        ui.list_helper.draw_list_ops(
+            col,
+            data,
+            'motionrefs_collection',
+            'motionrefs_collection_index',
+            custom_elements_func=draw_motion_refs_elements
+        )
+
+    # revision
+    _, box = ui.collapsible.draw(
+        object_box,
+        'object:revision',
+        'Revision'
+    )
+
+    if box:
+
+        # owner
+        split = utils.version.layout_split(box, 0.35)
+        split.label(text='Owner Name:')
+        split.prop(data.revision, 'owner', text='')
+
+        # created time
+        split = utils.version.layout_split(box, 0.35)
+        split.label(text='Created Time:')
+        split.prop(data.revision, 'ctime_str', text='')
+
+        # modif name
+        split = utils.version.layout_split(box, 0.35)
+        split.label(text='Moder Name:')
+        split.label(text=data.revision.moder)
+
+        # modified time
+        split = utils.version.layout_split(box, 0.35)
+        split.label(text='Modified Time:')
+        split.label(text=data.revision.mtime_str)
+
+        # time formats
+        subbox = box.box()
+        split = utils.version.layout_split(subbox, 0.25)
+        split.label(text='')
+        split.label(text='Time Formats:', icon='INFO')
+        subbox.label(text='Year.Month.Day Hours:Minutes')
+        subbox.label(text='Year.Month.Day')
+
+
+def draw_details_props(layout, context):
+    box = layout.box()
 
     if not (context.active_object.type in {'MESH', 'EMPTY'}):
         box.label(
@@ -212,7 +469,7 @@ def draw_split_prop_search(layout, owner, prop, label, search_owner, search_prop
     split.prop_search(owner, prop, search_owner, search_prop, text='')
 
 
-def level_draw_function(layout, data):
+def draw_level_props(layout, data):
     box = layout.box()
     level = data.level
     draw_split_prop(box, level, 'object_type', 'Type')
@@ -266,6 +523,7 @@ def level_draw_function(layout, data):
 
     elif object_type == 'VISUAL':
         draw_split_prop(box, level, 'visual_type', 'Visual Type')
+
         if level.visual_type in {'TREE_ST', 'TREE_PM'}:
             # color scale
             color_scale_box = box.box()
@@ -299,12 +557,16 @@ def level_draw_function(layout, data):
     elif object_type == 'LIGHT_DYNAMIC':
         draw_split_prop(box, level, 'controller_name', 'Controller')
         draw_split_prop(box, level, 'light_type_name', 'Light Type')
+
         row = box.row()
         row.prop(level, 'diffuse')
+
         row = box.row()
         row.prop(level, 'specular')
+
         row = box.row()
         row.prop(level, 'ambient')
+
         box.prop(level, 'range_')
         box.prop(level, 'falloff')
         box.prop(level, 'attenuation_0')
@@ -358,22 +620,29 @@ class XRAY_PT_object(ui.base.XRayPanel):
     @classmethod
     def poll(cls, context):
         bpy_obj = context.active_object
+
         if not bpy_obj:
             return False
+
         is_helper = utils.obj.is_helper_object(bpy_obj)
+
         if is_helper:
             return False
-        preferences = utils.version.get_preferences()
-        object_used, details_used, game_level_used = get_used(preferences)
+
+        pref = utils.version.get_preferences()
+        object_used, details_used, game_level_used = get_used(pref)
+
         return object_used or details_used or game_level_used
 
     def draw(self, context):
-        preferences = utils.version.get_preferences()
-        object_used, details_used, game_level_used = get_used(preferences)
+        pref = utils.version.get_preferences()
+        object_used, details_used, game_level_used = get_used(pref)
 
         layout = self.layout
         obj = context.active_object
         data = obj.xray
+
+        # object properties
         if object_used:
             layout.prop(
                 data,
@@ -384,168 +653,9 @@ class XRAY_PT_object(ui.base.XRayPanel):
             )
 
             if data.isroot:
-                object_box = layout.box()
-                if not data.flags_use_custom:
-                    split = utils.version.layout_split(object_box, 0.333)
-                    split.label(text='Type:')
-                    split.prop(data, 'flags_simple', text='')
-                else:
-                    # type
-                    split = utils.version.layout_split(object_box, 0.333)
-                    split.label(text='Type:')
-                    split.prop(data, 'flags_simple', text='')
-                    # static/dynamic
-                    row = object_box.row(align=True)
-                    row.prop(data, 'flags_custom_type', expand=True)
-                    # flags
-                    col = object_box.column(align=True)
-                    row = col.row(align=True)
-                    row.prop(data, 'flags_custom_progressive', text='Progressive', toggle=True)
-                    row.prop(data, 'flags_custom_lod', text='LOD', toggle=True)
-                    row.prop(data, 'flags_custom_hom', text='HOM', toggle=True)
-                    row = col.row(align=True)
-                    row.prop(data, 'flags_custom_musage', text='Multiple Usage', toggle=True)
-                    row.prop(data, 'flags_custom_soccl', text='Sound Occluder', toggle=True)
-                object_box.prop(data, 'flags_custom_hqexp', text='HQ Export')
-                split = utils.version.layout_split(object_box, 0.333)
-                split.label(text='LOD Reference:')
-                split.prop(data, 'lodref', text='')
-                split = utils.version.layout_split(object_box, 0.333)
-                split.label(text='Export Path:')
-                row = split.row(align=True)
-                row.prop(data, 'export_path', text='')
-                row.operator(
-                    ops.obj.XRAY_OT_set_export_path.bl_idname,
-                    text='',
-                    icon='FILE_FOLDER'
-                )
-                row, box = ui.collapsible.draw(
-                    object_box,
-                    'object:userdata',
-                    'User Data',
-                    enabled=data.userdata != '',
-                    icon='VIEWZOOM'
-                )
-                XRAY_OT_prop_clip.drawall(row, 'object.xray.userdata', data.userdata)
-                if box:
-                    if not data.userdata:
-                        ui.collapsible.XRAY_OT_collaps.set_value(
-                            'object:userdata',
-                            False
-                        )
-                    else:
-                        box = box.column(align=True)
-                        for line in data.userdata.splitlines():
-                            box.label(text=line)
+                draw_object_props(layout, data)
 
-                if data.motions:
-                    split = object_box.split()
-                    split.alert = True
-                    split.prop(data, 'motions')
-                _, box = ui.collapsible.draw(
-                    object_box,
-                    'object:motions',
-                    'Motions ({})'.format(len(data.motions_collection))
-                )
-                if box:
-                    col = box.column(align=True)
-                    col.prop(data, 'play_active_motion', toggle=True, icon='PLAY')
-                    col.prop(data, 'use_custom_motion_names', toggle=True, icon='SORTALPHA')
-                    names_row = col.row()
-                    names_row.label(text='Show:')
-                    names_row.prop(data, 'show_motions_names', expand=True)
-                    split = utils.version.layout_split(col, 0.333)
-                    split.label(text='Dependency:')
-                    split.prop_search(
-                        data,
-                        'dependency_object',
-                        bpy.data,
-                        'objects',
-                        text=''
-                    )
-                    row = box.row()
-                    row.template_list(
-                        ui.motion_list.XRAY_UL_motion_list.bl_idname,
-                        'name',
-                        data,
-                        'motions_collection',
-                        data,
-                        'motions_collection_index',
-                        rows=9
-                    )
-                    col = row.column(align=True)
-                    ui.list_helper.draw_list_ops(
-                        col,
-                        data,
-                        'motions_collection',
-                        'motions_collection_index',
-                        custom_elements_func=draw_motion_list_elements
-                    )
-
-                if data.motionrefs:
-                    split = object_box.split()
-                    split.alert = True
-                    split.prop(data, 'motionrefs')
-                _, box = ui.collapsible.draw(
-                    object_box,
-                    'object:motionsrefs',
-                    'Motion Refs ({})'.format(len(data.motionrefs_collection))
-                )
-                if box:
-                    box.prop(data, 'load_active_motion_refs', toggle=True)
-                    if data.load_active_motion_refs:
-                        row = box.row()
-                        row.label(text='Format:')
-                        row.prop(data.motions_browser, 'file_format', expand=True)
-                    row = box.row()
-                    row.template_list(
-                        'UI_UL_list',
-                        'name',
-                        data,
-                        'motionrefs_collection',
-                        data,
-                        'motionrefs_collection_index',
-                        rows=8
-                    )
-                    col = row.column(align=True)
-                    ui.list_helper.draw_list_ops(
-                        col,
-                        data,
-                        'motionrefs_collection',
-                        'motionrefs_collection_index',
-                        custom_elements_func=draw_motion_refs_elements
-                    )
-
-                _, box = ui.collapsible.draw(
-                    object_box,
-                    'object:revision',
-                    'Revision'
-                )
-                if box:
-                    # owner
-                    split = utils.version.layout_split(box, 0.35)
-                    split.label(text='Owner Name:')
-                    split.prop(data.revision, 'owner', text='')
-                    # created time
-                    split = utils.version.layout_split(box, 0.35)
-                    split.label(text='Created Time:')
-                    split.prop(data.revision, 'ctime_str', text='')
-                    # modif name
-                    split = utils.version.layout_split(box, 0.35)
-                    split.label(text='Moder Name:')
-                    split.label(text=data.revision.moder)
-                    # modified time
-                    split = utils.version.layout_split(box, 0.35)
-                    split.label(text='Modified Time:')
-                    split.label(text=data.revision.mtime_str)
-                    # time formats
-                    subbox = box.box()
-                    split = utils.version.layout_split(subbox, 0.25)
-                    split.label(text='')
-                    split.label(text='Time Formats:', icon='INFO')
-                    subbox.label(text='Year.Month.Day Hours:Minutes')
-                    subbox.label(text='Year.Month.Day')
-
+        # details properties
         if details_used:
             layout.prop(
                 data,
@@ -555,8 +665,9 @@ class XRAY_PT_object(ui.base.XRayPanel):
                 translate=False
             )
             if data.is_details:
-                details_draw_function(self, context)
+                draw_details_props(layout, context)
 
+        # level properties
         if game_level_used:
             layout.prop(
                 data,
@@ -566,7 +677,7 @@ class XRAY_PT_object(ui.base.XRayPanel):
                 translate=False
             )
             if data.is_level:
-                level_draw_function(layout, data)
+                draw_level_props(layout, data)
 
 
 classes = (
