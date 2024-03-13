e0d16a63b2e0267f1de0e3021e5391e3607e8d3a
PavelBlend <stalkermodkytia@yandex.ru>
2024-03-13 12:17:14 +0300

    tests for dm errors

--- tests/cases/test_dm_export.py
+++ tests/cases/test_dm_export.py
@@ -1,9 +1,9 @@
-from tests import utils
-
+import re
 import bpy
+import tests
 
 
-class TestDmExport(utils.XRayTestCase):
+class TestDmExport(tests.utils.XRayTestCase):
     def test_export_single(self):
         # Arrange
         self._create_dm_objects()
@@ -27,7 +27,7 @@ class TestDmExport(utils.XRayTestCase):
         # select objects
         names = set()
         for obj in bpy.data.objects:
-            utils.select_object(obj)
+            tests.utils.select_object(obj)
             names.add(obj.name)
 
         # Act
@@ -40,21 +40,133 @@ class TestDmExport(utils.XRayTestCase):
         # Assert
         self.assertOutputFiles({'tdm1.dm', 'tdm2.dm', 'tdm3.dm'})
 
+    def test_error_many_uv(self):
+        # Arrange
+        objs = self._create_dm_objects()
+        obj = objs[0]
+
+        uv_layer = obj.data.uv_layers.new(name='test')
+
+        # Act
+        bpy.ops.xray_export.dm_file(
+            detail_model=obj.name,
+            filepath=self.outpath('test_error.dm'),
+            texture_name_from_image_path=False
+        )
+
+        obj.data.uv_layers.remove(uv_layer)
+
+        # Assert
+        self.assertReportsNotContains('ERROR')
+        self.assertReportsContains(
+            'WARNING',
+            re.compile('Object has more than one UV-map. Active UV-map exported')
+        )
+
+    def test_error_no_uv(self):
+        # Arrange
+        objs = self._create_dm_objects()
+        obj = objs[0]
+
+        uv_layer = obj.data.uv_layers[0]
+        obj.data.uv_layers.remove(uv_layer)
+
+        # Act
+        bpy.ops.xray_export.dm_file(
+            detail_model=obj.name,
+            filepath=self.outpath('test_error.dm'),
+            texture_name_from_image_path=False
+        )
+
+        # Assert
+        self.assertReportsContains(
+            'ERROR',
+            re.compile('Mesh-object has no UV-map')
+        )
+
+    def test_error_many_materials(self):
+        # Arrange
+        objs = self._create_dm_objects()
+        obj = objs[0]
+
+        mat = bpy.data.materials.new('test')
+        obj.data.materials.append(mat)
+
+        # Act
+        bpy.ops.xray_export.dm_file(
+            detail_model=obj.name,
+            filepath=self.outpath('test_error.dm'),
+            texture_name_from_image_path=False
+        )
+
+        # Assert
+        self.assertReportsContains(
+            'ERROR',
+            re.compile('Mesh-object has more than one material')
+        )
+
+    def test_error_no_materials(self):
+        # Arrange
+        objs = self._create_dm_objects()
+        obj = objs[0]
+
+        obj.data.materials.clear()
+
+        # Act
+        bpy.ops.xray_export.dm_file(
+            detail_model=obj.name,
+            filepath=self.outpath('test_error.dm'),
+            texture_name_from_image_path=False
+        )
+
+        # Assert
+        self.assertReportsContains(
+            'ERROR',
+            re.compile('Object has no material')
+        )
+
+    def test_error_empty_material_slot(self):
+        # Arrange
+        objs = self._create_dm_objects()
+        obj = objs[0]
+
+        obj.data.materials.clear()
+        tests.utils.set_active_object(obj)
+        bpy.ops.object.material_slot_add()
+
+        # Act
+        bpy.ops.xray_export.dm_file(
+            detail_model=obj.name,
+            filepath=self.outpath('test_error.dm'),
+            texture_name_from_image_path=False
+        )
+
+        # Assert
+        self.assertReportsContains(
+            'ERROR',
+            re.compile('Object use empty material slot')
+        )
+
     def _create_dm_objects(self, create_uv=True, create_material=True):
-        bmesh = utils.create_bmesh((
-            (0, 0, 0),
-            (-1, -1, 0), (+1, -1, 0), (+1, +1, 0), (-1, +1, 0),
-        ), ((0, 1, 2), (0, 2, 3), (0, 3, 4), (0, 4, 1)), create_uv)
+        bmesh = tests.utils.create_bmesh(
+            # verts
+            ((0, 0, 0), (-1, -1, 0), (+1, -1, 0), (+1, +1, 0), (-1, +1, 0)),
+            # faces
+            ((0, 1, 2), (0, 2, 3), (0, 3, 4), (0, 4, 1)),
+            create_uv
+        )
 
         objs = []
+
         for i in range(3):
-            obj = utils.create_object(bmesh, create_material)
+            obj = tests.utils.create_object(bmesh, create_material)
             obj.name = 'tdm%d' % (i + 1)
             objs.append(obj)
             bpy_texture = bpy.data.textures.new('test_texture', 'IMAGE')
             bpy_image = bpy.data.images.new('test_image.dds', 0, 0)
             bpy_image.source = 'FILE'
             bpy_image.filepath = 'test_image.dds'
+
             if bpy.app.version >= (2, 80, 0):
                 obj.data.materials[0].use_nodes = True
                 node_tree = obj.data.materials[0].node_tree
@@ -66,8 +178,10 @@ class TestDmExport(utils.XRayTestCase):
                     texture_node.outputs['Color'],
                     princ_shader.inputs['Base Color']
                 )
+
             else:
                 bpy_texture.image = bpy_image
                 texture_slot = obj.data.materials[0].texture_slots.add()
                 texture_slot.texture = bpy_texture
+
         return objs
