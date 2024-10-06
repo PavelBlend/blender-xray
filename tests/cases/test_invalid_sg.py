4a0cca607e620c18b861a40f1c45f7f603fb1cbf
PavelBlend <stalkermodkytia@yandex.ru>
2024-10-06 19:45:30 +0300

    added tests for 'Check Smooth Groups Incompatibility' operator

--- tests/cases/test_invalid_sg.py
+++ tests/cases/test_invalid_sg.py
@@ -20,6 +20,105 @@ class TestInvalidSG(tests.utils.XRayTestCase):
         selected = [obj for obj in bpy.context.selected_objects]
         self.assertEqual(selected, [invalid_obj, ])
 
+    def test_incomp_sg(self):
+        # run without objects
+        bpy.ops.io_scene_xray.check_sg_incompatibility(mode='ACTIVE_OBJECT')
+
+        # create bad object
+        me_bad = bpy.data.meshes.new('bad')
+        ob_bad = bpy.data.objects.new('bad', me_bad)
+
+        # link and select bad object
+        tests.utils.link_object(ob_bad)
+        tests.utils.select_object(ob_bad)
+        tests.utils.set_active_object(ob_bad)
+
+        # add geometry
+        bpy.ops.object.mode_set(mode='EDIT')
+        bpy.ops.mesh.primitive_ico_sphere_add(subdivisions=1)
+        bpy.ops.object.mode_set(mode='OBJECT')
+
+        # set smoothing
+        me_bad.edges[0].use_edge_sharp = True
+
+        bpy.ops.object.select_all(action='DESELECT')
+
+        # create correct object
+        me_correct = bpy.data.meshes.new('correct')
+        ob_correct = bpy.data.objects.new('correct', me_correct)
+
+        # link and select correct object
+        tests.utils.link_object(ob_correct)
+        tests.utils.select_object(ob_correct)
+        tests.utils.set_active_object(ob_correct)
+
+        # add geometry
+        bpy.ops.object.mode_set(mode='EDIT')
+        bpy.ops.mesh.primitive_cube_add()
+        bpy.ops.object.mode_set(mode='OBJECT')
+
+        # set smoothing
+        for edge in me_correct.edges:
+            edge.use_edge_sharp = True
+
+        bpy.ops.object.select_all(action='DESELECT')
+
+        # create empty object
+        ob_emp = bpy.data.objects.new('empty', None)
+
+        # link empty object
+        tests.utils.link_object(ob_emp)
+
+        # select objects
+        tests.utils.select_object(ob_bad)
+        tests.utils.select_object(ob_correct)
+        tests.utils.select_object(ob_emp)
+        tests.utils.set_active_object(ob_correct)
+
+        # Act
+        bpy.ops.io_scene_xray.check_sg_incompatibility(mode='ACTIVE_OBJECT')
+
+        # Assert
+        self.assertEqual(len(bpy.context.selected_objects), 0)
+
+        # select objects
+        tests.utils.select_object(ob_bad)
+        tests.utils.select_object(ob_correct)
+        tests.utils.select_object(ob_emp)
+        tests.utils.set_active_object(ob_correct)
+
+        # Act
+        bpy.ops.io_scene_xray.check_sg_incompatibility(mode='SELECTED_OBJECTS')
+
+        # Assert
+        self.assertEqual(len(bpy.context.selected_objects), 1)
+        self.assertEqual(bpy.context.selected_objects[0].name, ob_bad.name)
+        sharp_edges = [
+            edge
+            for edge in bpy.context.selected_objects[0].data.edges
+                if edge.use_edge_sharp
+        ]
+        self.assertEqual(len(sharp_edges), 1)
+
+        # select objects
+        tests.utils.select_object(ob_bad)
+        tests.utils.select_object(ob_correct)
+        tests.utils.select_object(ob_emp)
+        tests.utils.set_active_object(ob_correct)
+
+        # Act
+        bpy.ops.io_scene_xray.check_sg_incompatibility(mode='ALL_OBJECTS')
+
+        # Assert
+        self.assertEqual(len(bpy.context.selected_objects), 1)
+        self.assertEqual(bpy.context.selected_objects[0].name, ob_bad.name)
+        sharp_edges = [
+            edge
+            for edge in bpy.context.selected_objects[0].data.edges
+                if edge.use_edge_sharp
+        ]
+        self.assertEqual(len(sharp_edges), 1)
+
     def _create_object(self, name, has_invalid_sg, empty):
         if empty:
             obj = bpy.data.objects.new(name, None)
