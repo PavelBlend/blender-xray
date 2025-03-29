2e8b671566a7e2abdd5daacf59709ba2edeb2bee
PavelBlend <stalkermodkytia@yandex.ru>
2025-03-29 11:45:39 +0300

    added tests for 'Change HQ Export' operator

--- tests/cases/test_ops_props.py
+++ tests/cases/test_ops_props.py
@@ -25,6 +25,9 @@ class TestOpsProps(tests.utils.XRayTestCase):
         # Arrange
         active, selected = self._create_objects()
 
+        for obj in bpy.data.objects:
+            obj.xray.flags_simple = 'st'
+
         self.default = {obj.name: obj.xray.flags_simple for obj in bpy.data.objects}
 
         # Act
@@ -81,10 +84,94 @@ class TestOpsProps(tests.utils.XRayTestCase):
         bpy.ops.io_scene_xray.change_object_type(mode='ALL', obj_type='st')
         self.assertReportsContains('ERROR', re.compile('No root-objects!'))
 
+    def test_change_hq_export(self):
+        tests.utils.remove_all_objects()
+
+        # tests without objects
+
+        self.clear_reports()
+        bpy.ops.io_scene_xray.change_hq_export(mode='ACTIVE', hq_export=True)
+        self.assertReportsContains('ERROR', re.compile('No active object!'))
+
+        self.clear_reports()
+        bpy.ops.io_scene_xray.change_hq_export(mode='SELECTED', hq_export=True)
+        self.assertReportsContains('ERROR', re.compile('No selected objects!'))
+
+        self.clear_reports()
+        bpy.ops.io_scene_xray.change_hq_export(mode='ALL', hq_export=True)
+        self.assertReportsContains('ERROR', re.compile('Scene has no objects!'))
+
+        # Arrange
+        active, selected = self._create_objects()
+
+        for obj in bpy.data.objects:
+            obj.xray.flags_custom_hqexp = False
+
+        self.default = {obj.name: obj.xray.flags_custom_hqexp for obj in bpy.data.objects}
+
+        # Act
+        self.clear_reports()
+        bpy.ops.io_scene_xray.change_hq_export(mode='ACTIVE', hq_export=True)
+        self.assertReportsContains('INFO', re.compile('Objects Changed: 1'))
+
+        # Assert
+        self.assertEqual(active.xray.flags_custom_hqexp, True)
+        for obj in bpy.data.objects:
+            if obj == active:
+                continue
+            self.assertEqual(obj.xray.flags_custom_hqexp, self.default[obj.name])
+        self._reset_hq_export()
+
+        # Act
+        self.clear_reports()
+        bpy.ops.io_scene_xray.change_hq_export(mode='SELECTED', hq_export=True)
+        self.assertReportsContains('INFO', re.compile('Objects Changed: 2'))
+
+        # Assert
+        for obj in selected:
+            self.assertEqual(obj.xray.flags_custom_hqexp, True)
+        for obj in bpy.data.objects:
+            if obj in selected:
+                continue
+            self.assertEqual(obj.xray.flags_custom_hqexp, self.default[obj.name])
+        self._reset_hq_export()
+
+        # Act
+        self.clear_reports()
+        bpy.ops.io_scene_xray.change_hq_export(mode='ALL', hq_export=True)
+        self.assertReportsContains('INFO', re.compile('Objects Changed: 5'))
+
+        # Assert
+        for obj in bpy.data.objects:
+            self.assertEqual(obj.xray.flags_custom_hqexp, True)
+        self._reset_hq_export()
+
+        # tests without root-objects
+
+        for obj in bpy.data.objects:
+            obj.xray.isroot = False
+
+        self.clear_reports()
+        bpy.ops.io_scene_xray.change_hq_export(mode='ACTIVE', hq_export=True)
+        self.assertReportsContains('ERROR', re.compile('No root-objects!'))
+
+        self.clear_reports()
+        bpy.ops.io_scene_xray.change_hq_export(mode='SELECTED', hq_export=True)
+        self.assertReportsContains('ERROR', re.compile('No root-objects!'))
+
+        self.clear_reports()
+        bpy.ops.io_scene_xray.change_hq_export(mode='ALL', hq_export=True)
+        self.assertReportsContains('ERROR', re.compile('No root-objects!'))
+
     def _reset_objects_type(self):
         for obj_name, obj_type in self.default.items():
             obj = bpy.data.objects[obj_name]
             obj.xray.flags_simple = obj_type
+
+    def _reset_hq_export(self):
+        for obj_name, hq_exp in self.default.items():
+            obj = bpy.data.objects[obj_name]
+            obj.xray.flags_custom_hqexp = hq_exp
 
     def _create_objects(self):
 
