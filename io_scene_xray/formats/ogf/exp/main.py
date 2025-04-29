981fca1dc508ce23aab3d203b978b99a965c9a65
PavelBlend <stalkermodkytia@yandex.ru>
2025-04-29 09:41:44 +0300

    refactor *.ogf export and fixed #840

--- io_scene_xray/formats/ogf/exp/main.py
+++ io_scene_xray/formats/ogf/exp/main.py
@@ -22,6 +22,83 @@ from .... import utils
 FIRST_SHADER = 0
 
 
+def write_header_child(mesh, chunked_writer):
+    # calculate bbox and bsphere
+    bbox_min, bbox_max = utils.mesh.calculate_mesh_bbox(mesh.verts)
+    bsph_ctr, bsph_rad = utils.mesh.calculate_mesh_bsphere(
+        (bbox_min, bbox_max),
+        mesh.verts
+    )
+
+    # create writer
+    header_writer = rw.write.PackedWriter()
+
+    # write
+    header_writer.putf('<B', fmt.FORMAT_VERSION_4)
+    header_writer.putf('<B', fmt.ModelType_v4.SKELETON_GEOMDEF_ST)
+    header_writer.putf('<H', FIRST_SHADER)
+    header_writer.putv3f(bbox_min)
+    header_writer.putv3f(bbox_max)
+    header_writer.putv3f(bsph_ctr)
+    header_writer.putf('<f', bsph_rad)
+
+    # write chunk
+    chunked_writer.put(fmt.HEADER, header_writer)
+
+
+def search_material(bpy_obj):
+    used_materials = utils.version.get_used_mats(bpy_obj.data)
+
+    # check without materials
+    if not len(bpy_obj.data.materials):
+        raise log.AppError(
+            text.error.obj_no_mat,
+            log.props(object=bpy_obj.name)
+        )
+
+    # check empty material slot
+    for mat_index in used_materials:
+        material = bpy_obj.data.materials[mat_index]
+        if not material:
+            raise log.AppError(
+                text.error.obj_empty_mat,
+                log.props(object=bpy_obj.name)
+            )
+
+    # collect object materials
+    materials = set()
+
+    for mat_index, material in enumerate(bpy_obj.data.materials):
+
+        if not material:
+            continue
+
+        if not mat_index in used_materials:
+            continue
+
+        materials.add(material)
+
+    materials = list(materials)
+
+    # check materials count
+    if not len(materials):
+        raise log.AppError(
+            text.error.obj_no_mat,
+            log.props(object=bpy_obj.name)
+        )
+
+    elif len(materials) > 1:
+        raise log.AppError(
+            text.error.many_mat,
+            log.props(object=bpy_obj.name)
+        )
+
+    material = materials[0]
+    two_sided = material.xray.flags_twosided
+
+    return material, two_sided
+
+
 def _export_child(
         root_obj,
         bpy_obj,
@@ -42,8 +119,6 @@ def _export_child(
         mods=modifiers
     )
 
-    bbox = utils.mesh.calculate_mesh_bbox(mesh.verts)
-    bsphere = utils.mesh.calculate_mesh_bsphere(bbox, mesh.verts)
     bmesh.ops.triangulate(mesh, faces=mesh.faces)
     bpy_mesh = bpy.data.meshes.new('.export-ogf')
     if not utils.version.IS_41:
@@ -52,45 +127,10 @@ def _export_child(
     mesh.to_mesh(bpy_mesh)
 
     # write header chunk
-    header_writer = rw.write.PackedWriter()
-    header_writer.putf('<B', fmt.FORMAT_VERSION_4)
-    header_writer.putf('<B', fmt.ModelType_v4.SKELETON_GEOMDEF_ST)
-    header_writer.putf('<H', 0)    # shader id
-    header_writer.putv3f(bbox[0])
-    header_writer.putv3f(bbox[1])
-    header_writer.putv3f(bsphere[0])
-    header_writer.putf('<f', bsphere[1])
-    chunked_writer.put(fmt.HEADER, header_writer)
+    write_header_child(mesh, chunked_writer)
 
     # search material
-    used_materials = utils.version.get_used_mats(bpy_obj.data)
-    for mat_index in used_materials:
-        material = bpy_obj.data.materials[mat_index]
-        if not material:
-            raise log.AppError(
-                text.error.obj_empty_mat,
-                log.props(object=bpy_obj.name)
-            )
-    materials = set()
-    for mat_index, material in enumerate(bpy_obj.data.materials):
-        if not material:
-            continue
-        if not mat_index in used_materials:
-            continue
-        materials.add(material)
-    materials = list(materials)
-    if not len(materials):
-        raise log.AppError(
-            text.error.obj_no_mat,
-            log.props(object=bpy_obj.name)
-        )
-    elif len(materials) > 1:
-        raise log.AppError(
-            text.error.many_mat,
-            log.props(object=bpy_obj.name)
-        )
-    material = materials[0]
-    two_sided = material.xray.flags_twosided
+    material, two_sided = search_material(bpy_obj)
 
     # generate texture path
     texture_path = utils.material.get_image_relative_path(
